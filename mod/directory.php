4a92de756395b403b13849917dcb519fe57db016
Mike Macgirvin <mike@macgirvin.com>
2010-07-09 17:39:55 -0700

    directory enhancements

--- mod/directory.php
+++ mod/directory.php
@@ -10,22 +10,44 @@ function directory_content(&$a) {
 
 	));
 
-	$r = q("SELECT `profile`.*, `profile`.`uid` AS `profile_uid`, `user`.`nickname` FROM `profile` LEFT JOIN `user` ON `user`.`uid` = `profile`.`uid` WHERE `is-default` = 1 AND `publish` = 1");
+	$r = q("SELECT `profile`.*, `profile`.`uid` AS `profile_uid`, `user`.`nickname` FROM `profile` LEFT JOIN `user` ON `user`.`uid` = `profile`.`uid` WHERE `is-default` = 1 AND `publish` = 1 ORDER BY `name` ASC");
 	if(count($r)) {
 
 		$tpl = file_get_contents('view/directory_item.tpl');
 
-		foreach($r as $rr) {
-			$profile_link = $a->get_baseurl() . '/profile/' . ((strlen($rr['nickname'])) ? $rr['nickname'] : $rr['profile_uid']);
+		if(in_array('small', $a->argv))
+			$photo = 'thumb';
+		else
+			$photo = 'photo';
 
+		foreach($r as $rr) {
+
+			$profile_link = $a->get_baseurl() . '/profile/' . ((strlen($rr['nickname'])) ? $rr['nickname'] : $rr['profile_uid']);
+			$details = '';
+			if(strlen($rr['locality']))
+				$details .= $rr['locality'];
+			if(strlen($rr['region'])) {
+				if(strlen($rr['locality']))
+					$details .= ', ';
+				$details .= $rr['region'];
+			}
+			if(strlen($rr['country-name'])) {
+				if(strlen($details))
+					$details .= ', ';
+				$details .= $rr['country-name'];
+			}
+			if(strlen($rr['dob']))
+				$details .= '<br />Age: ' ; // . calculate age($rr['dob'])) ;
+			if(strlen($rr['gender']))
+				$details .= '<br />Gender: ' . $rr['gender'];
 
 			$o .= replace_macros($tpl,array(
 				'$id' => $rr['id'],
 				'$profile-link' => $profile_link,
-				'$photo' => $rr['photo'],
+				'$photo' => $rr[$photo],
 				'$alt-text' => $rr['name'],
 				'$name' => $rr['name'],
-				'$details' => $details   // FIXME
+				'$details' => $details  
 
 
 			));
