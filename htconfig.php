c2106cb9df63a79ec9770a8f551053bcf192345f
Mike Macgirvin <mike@macgirvin.com>
2010-07-11 03:35:06 -0700

    add install notes

---
+++ htconfig.php
@@ -0,0 +1,24 @@
+<?php
+
+// Set the following for your MySQL installation
+// Copy or rename this file to .htconfig.php
+
+$db_host = 'your.mysqlhost.com';
+$db_user = 'mysqlusername';
+$db_pass = 'mysqlpassword';
+$db_data = 'mysqldatabasename';
+
+// Choose a legal default timezone. If you are unsure, use "America/Los_Angeles".
+// It can be changed later and only applies to timestamps for anonymous viewers.
+
+$default_timezone = 'Australia/Sydney';
+
+// What is your site name?
+
+$a->config['sitename'] = "DFRN developer";
+
+// At the present time you can have REGISTER_OPEN
+// or REGISTER_CLOSED. But register your personal account 
+// first before you close it.
+
+$a->config['register_policy'] = REGISTER_OPEN;
