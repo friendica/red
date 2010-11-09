9f606be54093b934081eb32f6d1a66f4d9fa9268
Friendika <info@friendika.com>
2010-11-08 21:10:53 -0800

    loosen search restrictions

--- mod/search.php
+++ mod/search.php
@@ -27,10 +27,11 @@ function search_content(&$a) {
 	$r = q("SELECT COUNT(*) AS `total`
 		FROM `item` LEFT JOIN `contact` ON `contact`.`id` = `item`.`contact-id`
 		WHERE `item`.`visible` = 1 AND `item`.`deleted` = 0
-		AND `wall` = 1
+		AND ( `wall` = 1 OR `contact`.`uid` = %d )
 		AND `contact`.`blocked` = 0 AND `contact`.`pending` = 0
 		AND MATCH (`item`.`body`) AGAINST ( '%s' IN BOOLEAN MODE )
 		$sql_extra ",
+		intval(local_user()),
 		dbesc($search)
 	);
 
@@ -50,11 +51,12 @@ function search_content(&$a) {
 		FROM `item` LEFT JOIN `contact` ON `contact`.`id` = `item`.`contact-id`
 		LEFT JOIN `user` ON `user`.`uid` = `item`.`uid` 
 		WHERE `item`.`visible` = 1 AND `item`.`deleted` = 0
-		AND `wall` = 1
+		AND ( `wall` = 1 OR `contact`.`uid` = %d )
 		AND `contact`.`blocked` = 0 AND `contact`.`pending` = 0
 		AND MATCH (`item`.`body`) AGAINST ( '%s' IN BOOLEAN MODE )
 		$sql_extra
 		ORDER BY `parent` DESC ",
+		intval(local_user()),
 		dbesc($search)
 	);
 
