1cffecd1bed6856dd40eddd8b0e6cd870d287762
Friendika <info@friendika.com>
2010-12-12 17:40:23 -0800

    footer is serving no useful purpose.

--- mod/home.php
+++ mod/home.php
@@ -14,12 +14,14 @@ if(! function_exists('home_content')) {
 function home_content(&$a) {
 
 	$o = '';
-	if(! (x($a->page,'footer')))
-		$a->page['footer'] = '';
-	$a->page['footer'] .= "<div class=\"powered\" >Powered by <a href=\"http://friendika.com\" title=\"friendika\" >friendika</a></div>";
+/*
+ *	if(! (x($a->page,'footer')))
+ *		$a->page['footer'] = '';
+ *	$a->page['footer'] .= "<div class=\"powered\" >Powered by <a href=\"http://friendika.com\" title=\"friendika\" >friendika</a></div>";
+ */
 	$o .= '<h1>Welcome' . ((x($a->config,'sitename')) ? " to {$a->config['sitename']}" : "" ) . '</h1>';
 	if(file_exists('home.html'))
-		$o .= file_get_contents('home.html');
+ 		$o .= file_get_contents('home.html');
 
 	$o .= login(($a->config['register_policy'] == REGISTER_CLOSED) ? 0 : 1);
 	return $o;
