b11963f9ac3551ff850ddfd4c8c58fe0c87dee1a
Friendika <info@friendika.com>
2011-01-11 18:46:37 -0800

    first checkin fortunate plugin

---
+++ addon/fortunate/fortunate.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+function fortunate_install() {
+	register_hook('page_end', 'addon/fortunate/fortunate.php', 'fortunate_fetch');
+}
+
+function fortunate_uninstall() {
+	unregister_hook('page_end', 'addon/fortunate/fortunate.php', 'fortunate_fetch');
+}
+
+
+function fortunate_fetch($a,&$b) {
+
+	$a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="' 
+		. $a->get_baseurl() . '/addon/fortunate/fortunate.css' . '" media="all" />' . "\r\n";
+
+	$s = fetch_url('http://fortunemod.com/cookie.php?numlines=2&equal=1&rand=' . mt_rand());
+	$b .= '<div class="fortunate">' . $s . '</div>';
+}
+
