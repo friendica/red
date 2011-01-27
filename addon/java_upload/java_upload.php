e8a3166fa9caa1bdff90526886ae705bc44c4ac8
Friendika <info@friendika.com>
2011-01-27 03:50:56 -0800

    Revert "roll version - now in alpha 2.1"
    This reverts commit df4f77dadddc7bd756c3e4f72fa2fbe204cfff88.

--- addon/java_upload/java_upload.php
+++ addon/java_upload/java_upload.php
@@ -3,47 +3,6 @@
 
 
 
-function java_upload_form(&$a,&$b) {
-
-
-$b .= <<< EOT;
-
-	<div id="photos-upload-select-files-text">$filestext</div>
-
-	<div id="photos_upload_applet_wrapper">
-		<applet name="jumpLoaderApplet"
-			code="jmaster.jumploader.app.JumpLoaderApplet.class"
-			archive="$archive"
-			width="700"
-			height="600"
-			mayscript >
-			<param name="uc_uploadUrl" value="$uploadurl" />
-			<param name="uc_uploadFormName" value="photos-upload-form" />
-			<param name="gc_loggingLeveL" value="FATAL" />
-			<param name="uc_fileParameterName" value="userfile" />
-			<param name="uc_cookie" value="PHPSESSID=$sessid; path=/;" />
-			<param name="vc_disableLocalFileSystem" value="false" />
-			<param name="vc_uploadViewMenuBarVisible" value="false" />
-			<param name="vc_mainViewFileListViewVisible" value="true" />
-			<param name="vc_mainViewFileListViewHeightPercent" value="50" />
-			<param name="vc_mainViewFileTreeViewVisible" value="true" />
-			<param name="vc_mainViewFileTreeViewWidthPercent" value="35" />
-			<param name="vc_lookAndFeel" value="system" />
-	
-		</applet>
-		
-	</div>
-
-	<div id="photos-upload-no-java-message" >
-	$nojava
-	</div>	
-
-EOT;
-
-}
-
-
-
 
 
 function java_upload_photo_post_init(&$a,&$b) {
--- boot.php
+++ boot.php
@@ -3,7 +3,7 @@
 set_time_limit(0);
 
 define ( 'BUILD_ID',               1033   );
-define ( 'FRIENDIKA_VERSION',      '2.10.0901' );
+define ( 'FRIENDIKA_VERSION',      '2.01.1006' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.0'  );
 
 define ( 'EOL',                    "<br />\r\n"     );
