f856493555480ae74f39f7d56796edc74ea61312
Friendika <info@friendika.com>
2011-02-15 03:13:38 -0800

    add permalink field to item table

--- boot.php
+++ boot.php
@@ -2,8 +2,8 @@
 
 set_time_limit(0);
 
-define ( 'BUILD_ID',               1038   );
-define ( 'FRIENDIKA_VERSION',      '2.10.0906' );
+define ( 'BUILD_ID',               1039   );
+define ( 'FRIENDIKA_VERSION',      '2.10.0907' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.1'  );
 
 define ( 'EOL',                    "<br />\r\n"     );
