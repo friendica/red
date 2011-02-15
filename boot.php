1b14f67a86c17da457af4c2ede78aec49f673195
Friendika <info@friendika.com>
2011-02-15 02:30:01 -0800

    bug #0000006, local sparkle links visible in "like" items when remote viewing

--- boot.php
+++ boot.php
@@ -1804,7 +1804,7 @@ function like_puller($a,$item,&$arr,$mode) {
 
 	if((activity_match($item['verb'],$verb)) && ($item['id'] != $item['parent'])) {
 		$url = $item['author-link'];
-		if(($item['network'] === 'dfrn') && (! $item['self']) && ($item['author-link'] == $item['url'])) {
+		if((local_user()) && (local_user() == $item['uid']) && ($item['network'] === 'dfrn') && (! $item['self']) && (link_compare($item['author-link'],$item['url']))) {
 			$url = $a->get_baseurl() . '/redir/' . $item['contact-id'];
 			$sparkle = ' class="sparkle" ';
 		}
