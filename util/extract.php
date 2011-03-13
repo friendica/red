d6b4940ddda4c9ac5cf4728a77a8bbf0d4c4ddf9
fabrixxm <fabrix.xm@gmail.com>
2011-03-13 13:51:59 +0100

    extract.php extract tt(). Updated strings.php

--- util/extract.php
+++ util/extract.php
@@ -10,25 +10,50 @@
 		$str = file_get_contents($file);
 
 		$pat = '| t\(([^\)]*)\)|';
+		$patt = '| tt\(([^\)]*)\)|';
 
 		preg_match_all($pat,$str,$matches);
+		preg_match_all($patt, $str, $matchestt);
+		
 
-		if(! count($matches))
-			continue;
-
-		foreach($matches[1] as $match) {
-			if(! in_array($match,$arr))
-				$arr[] = $match;
+		if(count($matches)){
+			foreach($matches[1] as $match) {
+				if(! in_array($match,$arr))
+					$arr[] = $match;
+			}
+		}
+		if(count($matchestt)){
+			foreach($matchestt[1] as $match) {
+				$matchtkns = preg_split("|[ \t\r\n]*,[ \t\r\n]*|",$match);
+				if (count($matchtkns)==3 && !in_array($matchtkns,$arr)){
+					$arr[] = $matchtkns;
+				}
+			}
 		}
 
 	}
 
 	$s = '<?php' . "\n";
-	foreach($arr as $a) {
-		if(substr($a,0,1) == '$')
-			continue;
+	$s .= '
+function string_plural_select($n){
+	return ($n != 1);
+}
 
-		$s .= '$a->strings[' . $a . '] = ' . $a . ';' . "\n";
+';
+
+	foreach($arr as $a) {
+		if (is_array($a)){
+			if(substr($a[1],0,1) == '$')
+				continue;
+			$s .= '$a->strings[' . $a[0] . "] = array(\n";
+			$s .= "\t0 => ". $a[0]. ",\n";
+			$s .= "\t1 => ". $a[1]. ",\n";
+			$s .= ");\n";
+		} else {
+			if(substr($a,0,1) == '$')
+				continue;			
+			$s .= '$a->strings[' . $a . '] = '. $a . ';' . "\n";
+		}
 	}
 
 	$zones = timezone_identifiers_list();
