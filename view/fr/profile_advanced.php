b8b77adad5cfcf5bad6802d95c1d0b01163d82dc
Olivier Migeot <olivier@migeot.org>
2011-03-21 14:02:01 +0100

    More templates frenchized

--- view/fr/profile_advanced.php
+++ view/fr/profile_advanced.php
@@ -4,7 +4,7 @@ $o .= '';
 
 $o .= <<< EOT
 
-<h2>Profile</h2>
+<h2>Profil</h2>
 
 
 EOT;
@@ -12,7 +12,7 @@ EOT;
 if($a->profile['name']) {
 $o .= <<< EOT
 <div id="advanced-profile-name-wrapper" >
-<div id="advanced-profile-name-text">Full Name:</div>
+<div id="advanced-profile-name-text">Nom complet:</div>
 <div id="advanced-profile-name">{$a->profile['name']}</div>
 </div>
 <div id="advanced-profile-name-end"></div>
@@ -22,7 +22,7 @@ EOT;
 if($a->profile['gender']) {
 $o .= <<< EOT
 <div id="advanced-profile-gender-wrapper" >
-<div id="advanced-profile-gender-text">Gender:</div>
+<div id="advanced-profile-gender-text">Genre:</div>
 <div id="advanced-profile-gender">{$a->profile['gender']}</div>
 </div>
 <div id="advanced-profile-gender-end"></div>
@@ -32,14 +32,14 @@ EOT;
 if(($a->profile['dob']) && ($a->profile['dob'] != '0000-00-00')) {
 $o .= <<< EOT
 <div id="advanced-profile-dob-wrapper" >
-<div id="advanced-profile-dob-text">Birthday:</div>
+<div id="advanced-profile-dob-text">Date de naissance/anniversaire:</div>
 EOT;
 
 // If no year, add an arbitrary one so just we can parse the month and day.
 
 $o .= '<div id="advanced-profile-dob">' 
 	. ((intval($a->profile['dob'])) 
-		? day_translate(datetime_convert('UTC','UTC',$a->profile['dob'],'j F, Y'))
+		? day_translate(datetime_convert('UTC','UTC',$a->profile['dob'],'j F Y'))
 		: day_translate(datetime_convert('UTC','UTC','2001-' . substr($a->profile['dob'],6),'j F'))) 
 	. "</div>\r\n</div>";
 
@@ -60,7 +60,7 @@ EOT;
 if($a->profile['marital']) {
 $o .= <<< EOT
 <div id="advanced-profile-marital-wrapper" >
-<div id="advanced-profile-marital-text"><span class="heart">&hearts;</span> Status:</div>
+<div id="advanced-profile-marital-text"><span class="heart">&hearts;</span> Statut:</div>
 <div id="advanced-profile-marital">{$a->profile['marital']}</div>
 EOT;
 
@@ -75,7 +75,7 @@ EOT;
 if($a->profile['sexual']) {
 $o .= <<< EOT
 <div id="advanced-profile-sexual-wrapper" >
-<div id="advanced-profile-sexual-text">Sexual Preference:</div>
+<div id="advanced-profile-sexual-text">Attirances sexuelles:</div>
 <div id="advanced-profile-sexual">{$a->profile['sexual']}</div>
 </div>
 <div id="advanced-profile-sexual-end"></div>
@@ -86,7 +86,7 @@ if($a->profile['homepage']) {
 	$homepage = linkify($a->profile['homepage']);
 $o .= <<< EOT
 <div id="advanced-profile-homepage-wrapper" >
-<div id="advanced-profile-homepage-text">Homepage:</div>
+<div id="advanced-profile-homepage-text">Site web:</div>
 <div id="advanced-profile-homepage">$homepage</div>
 </div>
 <div id="advanced-profile-homepage-end"></div>
@@ -96,7 +96,7 @@ EOT;
 if($a->profile['politic']) {
 $o .= <<< EOT
 <div id="advanced-profile-politic-wrapper" >
-<div id="advanced-profile-politic-text">Political Views:</div>
+<div id="advanced-profile-politic-text">Opinions politiques:</div>
 <div id="advanced-profile-politic">{$a->profile['politic']}</div>
 </div>
 <div id="advanced-profile-politic-end"></div>
@@ -116,7 +116,7 @@ EOT;
 if($txt = prepare_text($a->profile['about'])) {
 $o .= <<< EOT
 <div id="advanced-profile-about-wrapper" >
-<div id="advanced-profile-about-text">About:</div>
+<div id="advanced-profile-about-text">À propos:</div>
 <br />
 <div id="advanced-profile-about">$txt</div>
 </div>
@@ -127,7 +127,7 @@ EOT;
 if($txt = prepare_text($a->profile['interest'])) {
 $o .= <<< EOT
 <div id="advanced-profile-interest-wrapper" >
-<div id="advanced-profile-interest-text">Hobbies/Interests:</div>
+<div id="advanced-profile-interest-text">Marottes/Centres d'intérêt:</div>
 <br />
 <div id="advanced-profile-interest">$txt</div>
 </div>
@@ -138,7 +138,7 @@ EOT;
 if($txt = prepare_text($a->profile['contact'])) {
 $o .= <<< EOT
 <div id="advanced-profile-contact-wrapper" >
-<div id="advanced-profile-contact-text">Contact information and Social Networks:</div>
+<div id="advanced-profile-contact-text">Coordonées et réseaux sociaux:</div>
 <br />
 <div id="advanced-profile-contact">$txt</div>
 </div>
@@ -149,7 +149,7 @@ EOT;
 if($txt = prepare_text($a->profile['music'])) {
 $o .= <<< EOT
 <div id="advanced-profile-music-wrapper" >
-<div id="advanced-profile-music-text">Musical interests:</div>
+<div id="advanced-profile-music-text">Goûts musicaux:</div>
 <br />
 <div id="advanced-profile-music">$txt</div>
 </div>
@@ -160,7 +160,7 @@ EOT;
 if($txt = prepare_text($a->profile['book'])) {
 $o .= <<< EOT
 <div id="advanced-profile-book-wrapper" >
-<div id="advanced-profile-book-text">Books, literature:</div>
+<div id="advanced-profile-book-text">Livres, littérature:</div>
 <br />
 <div id="advanced-profile-book">$txt</div>
 </div>
@@ -171,7 +171,7 @@ EOT;
 if($txt = prepare_text($a->profile['tv'])) {
 $o .= <<< EOT
 <div id="advanced-profile-tv-wrapper" >
-<div id="advanced-profile-tv-text">Television:</div>
+<div id="advanced-profile-tv-text">Télévision:</div>
 <br />
 <div id="advanced-profile-tv">$txt</div>
 </div>
@@ -182,7 +182,7 @@ EOT;
 if($txt = prepare_text($a->profile['film'])) {
 $o .= <<< EOT
 <div id="advanced-profile-film-wrapper" >
-<div id="advanced-profile-film-text">Film/dance/culture/entertainment:</div>
+<div id="advanced-profile-film-text">Cinéma/Danse/Culture/Divertissement:</div>
 <br />
 <div id="advanced-profile-film">$txt</div>
 </div>
@@ -193,7 +193,7 @@ EOT;
 if($txt = prepare_text($a->profile['romance'])) {
 $o .= <<< EOT
 <div id="advanced-profile-romance-wrapper" >
-<div id="advanced-profile-romance-text">Love/romance:</div>
+<div id="advanced-profile-romance-text">Amour/Passion:</div>
 <br />
 <div id="advanced-profile-romance">$txt</div>
 </div>
@@ -204,7 +204,7 @@ EOT;
 if($txt = prepare_text($a->profile['work'])) {
 $o .= <<< EOT
 <div id="advanced-profile-work-wrapper" >
-<div id="advanced-profile-work-text">Work/employment:</div>
+<div id="advanced-profile-work-text">Travail/Activité professionnelle:</div>
 <br />
 <div id="advanced-profile-work">$txt</div>
 </div>
@@ -215,7 +215,7 @@ EOT;
 if($txt = prepare_text($a->profile['education'])) {
 $o .= <<< EOT
 <div id="advanced-profile-education-wrapper" >
-<div id="advanced-profile-education-text">School/education:</div>
+<div id="advanced-profile-education-text">École/études:</div>
 <br />
 <div id="advanced-profile-education">$txt</div>
 </div>
