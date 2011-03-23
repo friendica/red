7b59ea7b130f959460680e817f05bdf47764c96a
Fabio Comuni <fabrix.xm@gmail.com>
2011-03-23 15:32:18 +0100

    add some style to the widget

--- addon/widgets/widgets.php
+++ addon/widgets/widgets.php
@@ -160,6 +160,10 @@ function widget_friends_content(&$a, &$o, $conf){
 	if(!count($r)) return;
 	$a->profile = $r[0];
 
+	$o .= "<style>
+		.f9k_widget .contact-block-div { display: block !important; float: left!important; width: 50px!important; height: 50px!important; margin: 2px!important;}
+		.f9k_widget #contact-block-end { clear: left; }
+	</style>";
 	$o .= _abs_url(contact_block());
 	$o .= "<a href='".$a->get_baseurl().'/profile/'.$a->profile['nickname']."'>". t('Connect on Friendika!') ."</a>";
 	
