a5e0190f23a9b3ee840e98f9a819d688f6326781
Friendika <info@friendika.com>
2011-05-09 22:15:19 -0700

    missing self photo on remote site comment boxes

--- include/auth.php
+++ include/auth.php
@@ -28,6 +28,15 @@ if((isset($_SESSION)) && (x($_SESSION,'authenticated')) && ((! (x($_POST,'auth-p
 		goaway($a->get_baseurl());
 	}
 
+	if(x($_SESSION['visitor_id']) && (! x($_SESSION['uid']))) {
+		$r = q("SELECT * FROM `contact` WHERE `id` = %d LIMIT 1",
+			intval($_SESSION['visitor_id'])
+		);
+		if(count($r)) {
+			$a->contact = $r[0];
+		}
+	}
+
 	if(x($_SESSION,'uid')) {
 
 		// already logged in user returning
