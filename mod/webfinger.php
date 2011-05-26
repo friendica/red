7acb8fa6e7144435b81b57e3ad949846b1cbb68d
Friendika <info@friendika.com>
2011-05-26 16:41:36 -0700

    add webfinger diagnostic module

---
+++ mod/webfinger.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+function webfinger_content(&$a) {
+
+	$o .= '<h3>Webfinger Diagnostic</h3>';
+
+	$o .= '<form action="webfinger" method="get">';
+	$o .= 'Lookup address: <input type="text" style="width: 250px;" name="addr" value="' . $_GET['addr'] .'" />';
+	$o .= '<input type="submit" name="submit" value="Submit" /></form>'; 
+
+	$o .= '<br /><br />';
+
+	if(x($_GET,'addr')) {
+		$addr = $_GET['addr'];
+		if(strpos($addr,'@' !== false))
+			$res = webfinger($addr);
+		else
+			$res = lrdd($addr);
+		$o .= str_replace("\n",'<br />',print_r($res,true));
+	}
+	return $o;
+}
