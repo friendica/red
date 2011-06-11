d66056af9d56d86fdb57061c83c9187ea9f51403
Friendika <info@friendika.com>
2011-06-10 20:39:46 -0700

    reduce visibility of lang-select flag (duepuntozero)

--- boot.php
+++ boot.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 ini_set('pcre.backtrack_limit', 250000);
 
 
-define ( 'FRIENDIKA_VERSION',      '2.2.1006' );
+define ( 'FRIENDIKA_VERSION',      '2.2.1007' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.21'    );
 define ( 'DB_UPDATE_VERSION',      1063      );
 
