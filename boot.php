1441fce04eb0a95756661e54cbc22dff2e275c21
Friendika <info@friendika.com>
2011-07-25 20:21:16 -0700

    ignore any fully numeric hash tag

--- boot.php
+++ boot.php
@@ -2075,7 +2075,7 @@ function get_tags($s) {
 				continue;
 			}
 			// ignore strictly numeric tags like #1
-			if((strpos($mtch,'#') === 0) && ctype_digit(substr($mtch,1)))
+			if((strpos($mtch,'#') === 0) && ctype_digit($mtch))
 				continue;
 			if(substr($mtch,-1,1) === '.')
 				$ret[] = substr($mtch,0,-1);
