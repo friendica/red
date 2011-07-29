ef47a1cf973a66bb22c1d72dc2a3130db9324e1f
Friendika <info@friendika.com>
2011-07-29 16:05:16 -0700

    revup

--- boot.php
+++ boot.php
@@ -1,6 +1,6 @@
 <?php
 
-define ( 'FRIENDIKA_VERSION',      '2.2.1055' );
+define ( 'FRIENDIKA_VERSION',      '2.2.1056' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.21'    );
 define ( 'DB_UPDATE_VERSION',      1076      );
 
