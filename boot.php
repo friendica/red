aa3a14ec36db5f9df9d0ec27c607a994451fc845
Friendika <info@friendika.com>
2011-08-05 21:30:12 -0700

    bug #117, use realname for screen_name in API if nickname missing

--- boot.php
+++ boot.php
@@ -7,7 +7,7 @@ require_once('include/text.php');
 require_once("include/pgettext.php");
 
 
-define ( 'FRIENDIKA_VERSION',      '2.2.1062' );
+define ( 'FRIENDIKA_VERSION',      '2.2.1063' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.21'    );
 define ( 'DB_UPDATE_VERSION',      1077      );
 
