0db145dafa032a8c1fd68c318afc59f9599f3756
Friendika <info@friendika.com>
2011-08-11 04:28:06 -0700

    redirect to home after logging in using login url

--- mod/login.php
+++ mod/login.php
@@ -3,6 +3,8 @@
 function login_content(&$a) {
 	if(x($_SESSION,'theme'))
 		unset($_SESSION['theme']);
+	if(local_user())
+		goaway(z_root());
 	return login(($a->config['register_policy'] == REGISTER_CLOSED) ? false : true);
 
 }
