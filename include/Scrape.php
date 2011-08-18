69b7c2f0becdda1e331eef0069e163c2dad9df0e
Friendika <info@friendika.com>
2011-08-17 23:10:55 -0700

    documentation

--- include/Scrape.php
+++ include/Scrape.php
@@ -301,6 +301,26 @@ function scrape_feed($url) {
 	return $ret;
 }}
 
+
+/**
+ *
+ * Probe a network address to discover what kind of protocols we need to communicate with it.
+ *
+ * Warning: this function is a bit touchy and there are some subtle dependencies within the logic flow.
+ * Edit with care.
+ *
+ */
+
+/**
+ *
+ * PROBE_DIASPORA has a bias towards returning Diaspora information
+ * while PROBE_NORMAL has a bias towards dfrn/zot - in the case where
+ * an address (such as a Friendika address) supports more than one type
+ * of network. 
+ *
+ */
+
+
 define ( 'PROBE_NORMAL',   0);
 define ( 'PROBE_DIASPORA', 1);
 
