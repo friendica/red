1b33ba6587766d939ac37c1ca60aa5c012e4da9d
Friendika <info@friendika.com>
2011-08-25 06:01:40 -0700

    third attempt to get rid of this stupid warning

--- include/network.php
+++ include/network.php
@@ -536,7 +536,7 @@ function fetch_xrd_links($url) {
 			$aliases = array($alias);
 		else
 			$aliases = $alias;
-		if($aliases && count($aliases)) {
+		if(is_array($aliases) && count($aliases)) {
 			foreach($aliases as $alias) {
 				$links[]['@attributes'] = array('rel' => 'alias' , 'href' => $alias);
 			}
