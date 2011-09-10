ebb9af8946b68a545d9a7b5a6763721627a981f7
Friendika <info@friendika.com>
2011-09-09 17:35:26 -0700

    One day we'll have ignored all purely numeric hashtags like #33. Found yet another edge case.

--- include/text.php
+++ include/text.php
@@ -482,13 +482,12 @@ function get_tags($s) {
 				// we might be inside a bbcode color tag - leave it alone
 				continue;
 			}
+			if(substr($mtch,-1,1) === '.')
+				$mtch = substr($mtch,0,-1);
 			// ignore strictly numeric tags like #1
 			if((strpos($mtch,'#') === 0) && ctype_digit(substr($mtch,1)))
 				continue;
-			if(substr($mtch,-1,1) === '.')
-				$ret[] = substr($mtch,0,-1);
-			else
-				$ret[] = $mtch;
+			$ret[] = $mtch;
 		}
 	}
 	return $ret;
