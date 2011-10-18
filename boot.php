b14914ee2ea7317a564386d287fc2d0f271e4832
Friendika <info@friendika.com>
2011-10-18 13:07:27 -0700

    fix tb tabs

--- boot.php
+++ boot.php
@@ -8,7 +8,7 @@ require_once("include/pgettext.php");
 require_once('include/nav.php');
 
 define ( 'FRIENDIKA_PLATFORM',     'Free Friendika');
-define ( 'FRIENDIKA_VERSION',      '2.3.1137' );
+define ( 'FRIENDIKA_VERSION',      '2.3.1138' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.21'    );
 define ( 'DB_UPDATE_VERSION',      1097      );
 
