0eceade2fe0bb432543d8c48b3e9e6c5f3a3b00c
Friendika <info@friendika.com>
2011-10-30 16:28:07 -0700

    view source of item [bbcode] usage: viewsrc/nnn

---
+++ mod/viewsrc.php
@@ -0,0 +1,30 @@
+<?php
+
+
+function viewsrc_content(&$a) {
+
+	if(! local_user()) {
+		notice( t('Access denied.') . EOL);
+		return;
+	}
+
+	$item_id = (($a->argc > 1) ? intval($a->argv[1]) : 0);
+
+	if(! $item_id) {
+		$a->error = 404;
+		notice( t('Item not found.') . EOL);
+		return;
+	}
+
+	$r = q("SELECT `item`.`body` FROM `item` 
+		WHERE `item`.`uid` = %d AND `item`.`visible` = 1 AND `item`.`deleted` = 0
+		AND `item`.`id` = '%s' LIMIT 1",
+		intval(local_user()),
+		dbesc($item_id)
+	);
+
+	if(count($r))
+		$o .= str_replace("\n",'<br />',$r[0]['body']);
+	return $o;
+}
+
