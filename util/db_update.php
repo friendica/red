e9fb809a2dad29af1c195d86d8891312bac3c09f
friendica <info@friendica.com>
2012-01-15 15:02:48 -0800

    add db_update cmdline script

---
+++ util/db_update.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once("boot.php");
+
+$a = new App;
+@include(".htconfig.php");
+
+$lang = get_language();
+load_translation_table($lang);
+
+require_once("dba.php");
+$db = new dba($db_host, $db_user, $db_pass, $db_data, false);
+        unset($db_host, $db_user, $db_pass, $db_data);
+
+$build = get_config('system','build');
+
+echo "Old DB VERSION: " . $build . "\n";
+echo "New DB VERSION: " . DB_UPDATE_VERSION . "\n";
+
+
+if($build != DB_UPDATE_VERSION) {
+	echo "Updating database...";
+	check_config();
+	echo "Done\n";
+}
+
