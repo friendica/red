4fc455d195fef9f1bfbc9f125788650fb3153237
friendica <info@friendica.com>
2012-01-31 15:50:49 -0800

    revup

--- boot.php
+++ boot.php
@@ -9,7 +9,7 @@ require_once('include/nav.php');
 require_once('include/cache.php');
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
-define ( 'FRIENDICA_VERSION',      '2.3.1240' );
+define ( 'FRIENDICA_VERSION',      '2.3.1241' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.22'    );
 define ( 'DB_UPDATE_VERSION',      1118      );
 
