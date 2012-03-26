37f10a6d4fd2f8f5858f3222269bd6f48031962f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2012-03-26 14:48:01 +0200

    marking quattro-green as extension of quattro and deleting the tpl files

---
+++ view/theme/quattro-green/theme.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Name: quattro-green
+ * Version: 1.0
+ * Author: Fabio Communi <fabrix.xm@gmail.com>
+ * Maintainer: Tobias Diekershoff
+ */
+$a->theme_info = array(
+  'extends' => 'quattro',
+);
+EOT;
