be2005f146fc8d482734ca48ec594bc3f797360c
Fabio Comuni <fabrix.xm@gmail.com>
2012-03-30 21:21:14 +0200

    quattro: move quattro-green inside quattro folder. Add config option to select quattro color scheme.

--- view/theme/quattro/config.php
+++ view/theme/quattro/config.php
@@ -10,6 +10,11 @@ function theme_content(&$a){
 		return;		
 	
 	$align = get_pconfig(local_user(), 'quattro', 'align' );
+	$color = get_pconfig(local_user(), 'quattro', 'color' );
+	$colors = array(
+		"dark"=>"Quattro", 
+		"green"=>"Green"
+	);
 	
 	$t = file_get_contents( dirname(__file__). "/theme_settings.tpl" );
 	$o .= replace_macros($t, array(
@@ -17,6 +22,7 @@ function theme_content(&$a){
 		'$baseurl' => $a->get_baseurl(),
 		'$title' => t("Theme settings"),
 		'$align' => array('quattro_align', t('Alignment'), $align, '', array('left'=>t('Left'), 'center'=>t('Center'))),
+		'$color' => array('quattro_color', t('Color scheme'), $color, '', $colors),
 	));
 	return $o;
 }
@@ -24,8 +30,10 @@ function theme_content(&$a){
 function theme_post(&$a){
 	if(! local_user())
 		return;
+	
 	if (isset($_POST['quattro-settings-submit'])){
 		set_pconfig(local_user(), 'quattro', 'align', $_POST['quattro_align']);
+		set_pconfig(local_user(), 'quattro', 'color', $_POST['quattro_color']);
 	}
 }
 
--- view/theme/quattro/theme_settings.tpl
+++ view/theme/quattro/theme_settings.tpl
@@ -1,3 +1,5 @@
+{{inc field_select.tpl with $field=$color}}{{endinc}}
+
 {{inc field_select.tpl with $field=$align}}{{endinc}}
 
 <div class="settings-submit-wrapper">
