24e7a3413c92c516e662f0c418fde992a9f6c9da
Fabio Comuni <fabrix.xm@gmail.com>
2012-03-30 15:46:14 +0200

    quattro-green: remove settings code Use config.php from quattro

--- view/theme/quattro-green/theme.php
+++ view/theme/quattro-green/theme.php
@@ -10,35 +10,6 @@ $a->theme_info = array(
   'extends' => 'quattro',
 );
 
-$a->hooks[] = array('plugin_settings', 'view/theme/quattro-green/theme.php', 'quattro_green_settings');
-$a->hooks[] = array('plugin_settings_post', 'view/theme/quattro-green/theme.php', 'quattro_green_settings_post');
-
-
-function quattro_green_settings(&$a, &$o){
-	if(!local_user())
-		return;		
-	
-	$align = get_pconfig(local_user(), 'quattro', 'align' );
-	
-	$t = file_get_contents( dirname(__file__). "/theme_settings.tpl" );
-	$o .= replace_macros($t, array(
-		'$submit' => t('Submit'),
-		'$baseurl' => $a->get_baseurl(),
-		'$title' => t("Theme settings"),
-		'$align' => array('quattro_align', t('Alignment'), $align, '', array('left'=>t('Left'), 'center'=>t('Center'))),
-	));
-}
-
-function quattro_green_settings_post(&$a){
-	if(! local_user())
-		return;
-	if (isset($_POST['quattro-settings-submit'])){
-		set_pconfig(local_user(), 'quattro', 'align', $_POST['quattro_align']);
-	}
-	goaway($a->get_baseurl()."/settings/addon");
-}
-
-
 $quattro_align = get_pconfig(local_user(), 'quattro', 'align' );
 
 if(local_user() && $quattro_align=="center"){
