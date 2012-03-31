53f799f2bc125fadf527e3c65e8e9882d53d9aea
friendica <info@friendica.com>
2012-03-31 15:25:17 -0700

    handle escaped file chars in display and search

--- include/contact_widgets.php
+++ include/contact_widgets.php
@@ -87,7 +87,7 @@ function fileas_widget($baseurl,$selected = '') {
     $cnt = preg_match_all('/\[(.*?)\]/',$saved,$matches,PREG_SET_ORDER);
     if($cnt) {
 		foreach($matches as $mtch) {
-			$unescaped = file_tag_decode($mtch[1]);
+			$unescaped = xmlify(file_tag_decode($mtch[1]));
 			$terms[] = array('name' => $unescaped,'selected' => (($selected == $unescaped) ? 'selected' : ''));
 		}
 	}
