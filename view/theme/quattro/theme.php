7aa3b92f4c4619de76f45f448edfd6e4dcfbceb4
Fabio Comuni <fabrix.xm@gmail.com>
2012-04-13 11:21:15 +0200

    quattro: add admin theme settigs. darker items background with a bottom border.

---
+++ view/theme/quattro/theme.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Name: Quattro
+ * Author: Fabio <http://kirgroup.com/profile/fabrixxm>
+ * Maintainer: Fabio <http://kirgroup.com/profile/fabrixxm>
+ * Maintainer: Tobias <https://diekershoff.homeunix.net/friendika/profile/tobias>
+ */
+ 
+	$a->theme_info = array();
+ 
+
