f5f1956e8b855ab5aeab531fc44a1a773427bf64
tommy tomson <thomas.bierey@gmx.de>
2012-04-20 01:44:20 +0200

    diabook-themes: theme-settings: add "colour scheme"-option, add diabook-pink and some bug/css-fixes

--- view/theme/diabook/config.php
+++ view/theme/diabook/config.php
@@ -12,8 +12,9 @@ function theme_content(&$a){
 	$font_size = get_pconfig(local_user(), 'diabook', 'font_size' );
 	$line_height = get_pconfig(local_user(), 'diabook', 'line_height' );
 	$resolution = get_pconfig(local_user(), 'diabook', 'resolution' );
+	$color = get_pconfig(local_user(), 'diabook', 'color' );
 	
-	return diabook_form($a,$font_size, $line_height, $resolution);
+	return diabook_form($a,$font_size, $line_height, $resolution, $color);
 }
 
 function theme_post(&$a){
@@ -24,6 +25,7 @@ function theme_post(&$a){
 		set_pconfig(local_user(), 'diabook', 'font_size', $_POST['diabook_font_size']);
 		set_pconfig(local_user(), 'diabook', 'line_height', $_POST['diabook_line_height']);
 		set_pconfig(local_user(), 'diabook', 'resolution', $_POST['diabook_resolution']);
+		set_pconfig(local_user(), 'diabook', 'color', $_POST['diabook_color']);	
 	}
 }
 
@@ -32,8 +34,9 @@ function theme_admin(&$a){
 	$font_size = get_config('diabook', 'font_size' );
 	$line_height = get_config('diabook', 'line_height' );
 	$resolution = get_config('diabook', 'resolution' );
+	$color = get_config('diabook', 'color' );	
 	
-	return diabook_form($a,$font_size, $line_height, $resolution);
+	return diabook_form($a,$font_size, $line_height, $resolution, $color);
 }
 
 function theme_admin_post(&$a){
@@ -41,11 +44,12 @@ function theme_admin_post(&$a){
 		set_config('diabook', 'font_size', $_POST['diabook_font_size']);
 		set_config('diabook', 'line_height', $_POST['diabook_line_height']);
 		set_config('diabook', 'resolution', $_POST['diabook_resolution']);
+		set_config('diabook', 'color', $_POST['diabook_color']);
 	}
 }
 
 
-function diabook_form(&$a, $font_size, $line_height, $resolution){
+function diabook_form(&$a, $font_size, $line_height, $resolution, $color){
 	$line_heights = array(
 		"1.3"=>"1.3",
 		"---"=>"---",
@@ -68,6 +72,13 @@ function diabook_form(&$a, $font_size, $line_height, $resolution){
 		'normal'=>'normal',
 		'wide'=>'wide',		
 		);
+	$colors = array(
+		'diabook'=>'diabook',
+		'aerith'=>'aerith',		
+		'blue'=>'blue',		
+		'red'=>'red',
+		'pink'=>'pink',				
+		);
 	
 	
 	
@@ -79,6 +90,7 @@ function diabook_form(&$a, $font_size, $line_height, $resolution){
 		'$font_size' => array('diabook_font_size', t('Set font-size for posts and comments'), $font_size, '', $font_sizes),
 		'$line_height' => array('diabook_line_height', t('Set line-height for posts and comments'), $line_height, '', $line_heights),
 		'$resolution' => array('diabook_resolution', t('Set resolution for middle column'), $resolution, '', $resolutions),
+		'$color' => array('diabook_color', t('Set color scheme'), $color, '', $colors),	
 	));
 	return $o;
 }
---
+++ view/theme/diabook/diabook-pink/group_side.tpl
@@ -0,0 +1,34 @@
+<div id="profile_side" >
+	<div class="">
+		<h3 style="margin-left: 2px;">$title<a href="group/new" title="$createtext" class="icon text_add"></a></h3>
+	</div>
+
+	<div id="sidebar-group-list">
+		<ul class="menu-profile-side">
+			{{ for $groups as $group }}
+			<li class="menu-profile-list">
+				<span class="menu-profile-icon {{ if $group.selected }}group_selected{{else}}group_unselected{{ endif }}"></span>
+				<a href="$group.href" class="menu-profile-list-item">
+					$group.text
+				</a>
+				{{ if $group.edit }}
+					<a href="$group.edit.href" class="action"><span class="icon text_edit" ></span></a>
+				{{ endif }}
+				{{ if $group.cid }}
+					<input type="checkbox" 
+						class="{{ if $group.selected }}ticked{{ else }}unticked {{ endif }} action" 
+						onclick="contactgroupChangeMember('$group.id','$group.cid');return true;"
+						{{ if $group.ismember }}checked="checked"{{ endif }}
+					/>
+				{{ endif }}
+			</li>
+			{{ endfor }}
+		</ul>
+	</div>
+	 {{ if $ungrouped }}
+  <div id="sidebar-ungrouped">
+  <a href="nogroup">$ungrouped</a>
+  </div>
+  {{ endif }}
+</div>	
+
