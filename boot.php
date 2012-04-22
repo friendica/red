00cb7f9efb4826a6ad5c0791f26bc5e8db644f77
friendica <info@friendica.com>
2012-04-22 16:24:29 -0700

    don't allow non-existent themes as choices

--- boot.php
+++ boot.php
@@ -9,7 +9,7 @@ require_once('include/nav.php');
 require_once('include/cache.php');
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
-define ( 'FRIENDICA_VERSION',      '2.3.1319' );
+define ( 'FRIENDICA_VERSION',      '2.3.1320' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
 define ( 'DB_UPDATE_VERSION',      1138      );
 
