3ecde2d72f27c248eb13d53d3f601934f6baaf16
friendica <info@friendica.com>
2012-05-02 16:27:19 -0700

    use mysql replace on cache items

--- include/cache.php
+++ include/cache.php
@@ -14,22 +14,40 @@
 		}
 		
 		public static function set($key,$value) {
-			$r = q("SELECT * FROM `cache` WHERE `k`='%s' limit 1",
-				dbesc($key)
-			);
-			if(count($r)) {
-				q("UPDATE `cache` SET `v` = '%s', `updated = '%s' WHERE `k` = '%s' limit 1",
-					dbesc($value),
-					dbesc(datetime_convert()),
-					dbesc($key));
-			}
-			else {
-				q("INSERT INTO `cache` (`k`,`v`,`updated`) VALUES ('%s','%s','%s')",
+
+			q("REPLACE INTO `cache` (`k`,`v`,`updated`) VALUES ('%s','%s','%s')",
 					dbesc($key),
 					dbesc($value),
 					dbesc(datetime_convert()));
-			}
 		}
+
+		
+/* 
+ *
+ * Leaving this legacy code temporaily to see how REPLACE fares 
+ * as opposed to non-atomic checks when faced with fast moving key duplication.
+ * As a MySQL extension it isn't portable, but we're not yet very portable.
+ */
+
+/*
+ *			$r = q("SELECT * FROM `cache` WHERE `k`='%s' limit 1",
+ *				dbesc($key)
+ *			);
+ *			if(count($r)) {
+ *				q("UPDATE `cache` SET `v` = '%s', `updated = '%s' WHERE `k` = '%s' limit 1",
+ *					dbesc($value),
+ *					dbesc(datetime_convert()),
+ *					dbesc($key));
+ *			}
+ *			else {
+ *				q("INSERT INTO `cache` (`k`,`v`,`updated`) VALUES ('%s','%s','%s')",
+ *					dbesc($key),
+ *					dbesc($value),
+ *					dbesc(datetime_convert()));
+ *			}
+ *		}
+ */
+
 		
 		public static function clear(){
 			q("DELETE FROM `cache` WHERE `updated` < '%s'",
