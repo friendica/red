7f061c255ce4e3b41606509fcf83a98fe8c63202
Christian Vogeley <christian.vogeley@hotmail.de>
2012-05-05 00:45:35 +0200

    Option to set theme width added. Minor layout changes

--- view/theme/cleanzero/config.php
+++ view/theme/cleanzero/config.php
@@ -12,8 +12,9 @@ function theme_content(&$a){
 	$resize = get_pconfig(local_user(), 'cleanzero', 'resize' );
 	$color = get_pconfig(local_user(), 'cleanzero', 'color' );
        $font_size = get_pconfig(local_user(), 'cleanzero', 'font_size' );
+	$theme_width= get_pconfig(local_user(), 'cleanzero', 'theme_width' );
 	
-	return cleanzero_form($a,$color,$font_size,$resize);
+	return cleanzero_form($a,$color,$font_size,$resize,$theme_width);
 }
 
 function theme_post(&$a){
@@ -24,6 +25,7 @@ function theme_post(&$a){
 		set_pconfig(local_user(), 'cleanzero', 'resize', $_POST['cleanzero_resize']);	
 		set_pconfig(local_user(), 'cleanzero', 'color', $_POST['cleanzero_color']);
 		set_pconfig(local_user(), 'cleanzero', 'font_size', $_POST['cleanzero_font_size']);
+		set_pconfig(local_user(), 'cleanzero', 'theme_width', $_POST['cleanzero_theme_width']);
 	}
 }
 
@@ -32,8 +34,8 @@ function theme_admin(&$a){
 	$resize = get_config('cleanzero', 'resize' );
 	$color = get_config('cleanzero', 'color' );
 	$font_size = get_config('cleanzero', 'font_size' );
-	
-	return cleanzero_form($a,$color,$font_size,$resize);
+	$theme_width= get_config('cleanzero', 'theme_width' );
+	return cleanzero_form($a,$color,$font_size,$resize,$theme_width);
 }
 
 function theme_admin_post(&$a){
@@ -41,11 +43,13 @@ function theme_admin_post(&$a){
 		set_config('cleanzero', 'resize', $_POST['cleanzero_resize']);
 		set_config('cleanzero', 'color', $_POST['cleanzero_color']);
 		set_config('cleanzero', 'font_size', $_POST['cleanzero_font_size']);
+		set_config('cleanzero', 'theme_width', $_POST['cleanzero_theme_width']);
+
 	}
 }
 
 
-function cleanzero_form(&$a, $color,$font_size,$resize){
+function cleanzero_form(&$a, $color,$font_size,$resize,$theme_width){
 	$colors = array(
 		"cleanzero"=>"cleanzero", 
 		"cleanzero-green"=>"green",
@@ -65,7 +69,12 @@ function cleanzero_form(&$a, $color,$font_size,$resize){
 		"250"=>"3 (250px)",
 		"150"=>"4 (150px)",
 	       );
-	
+	$theme_widths =array (
+		"standard"=>"standard",
+		"narrow"=>"narrow",
+		"wide"=>"wide",
+		);
+
 	$t = file_get_contents( dirname(__file__). "/theme_settings.tpl" );
 	$o .= replace_macros($t, array(
 		'$submit' => t('Submit'),
@@ -73,6 +82,7 @@ function cleanzero_form(&$a, $color,$font_size,$resize){
 		'$title' => t("Theme settings"),
 		'$resize' => array('cleanzero_resize',t ('Set resize level for images in posts and comments (width and height)'),$resize,'',$resizes),
 		'$font_size' => array('cleanzero_font_size', t('Set font-size for posts and comments'), $font_size, '', $font_sizes),
+		'$theme_width' => array('cleanzero_theme_width', t('Set theme width'), $theme_width, '', $theme_widths),
 		'$color' => array('cleanzero_color', t('Color scheme'), $color, '', $colors),
 	));
 	return $o;
