10754ced3e51f796102005b9661b747c06fd4e0b
friendica <info@friendica.com>
2012-05-04 06:42:56 -0700

    category removal

--- mod/filerm.php
+++ mod/filerm.php
@@ -7,12 +7,18 @@ function filerm_content(&$a) {
 	}
 
 	$term = unxmlify(trim($_GET['term']));
+	$cat = unxmlify(trim($_GET['cat']));
+
+	$category = (($cat) ? true : false);
+	if($category)
+		$term = $cat;
+
 	$item_id = (($a->argc > 1) ? intval($a->argv[1]) : 0);
 
 	logger('filerm: tag ' . $term . ' item ' . $item_id);
 
 	if($item_id && strlen($term))
-		file_tag_unsave_file(local_user(),$item_id,$term);
+		file_tag_unsave_file(local_user(),$item_id,$term, $category);
 
 	if(x($_SESSION,'return_url'))
 		goaway($a->get_baseurl() . '/' . $_SESSION['return_url']);
