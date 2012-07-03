7926a0d171e1e84dcc551f67ef0d8f7bfac4a19f
friendica <info@friendica.com>
2012-07-03 16:26:14 -0700

    add link to shares

--- mod/share.php
+++ mod/share.php
@@ -23,7 +23,9 @@ function share_init(&$a) {
 	$o .= "\xE2\x99\xb2" . ' [url=' . $r[0]['author-link'] . ']' . $r[0]['author-name'] . '[/url]' . "\n";
 	if($r[0]['title'])
 		$o .= '[b]' . $r[0]['title'] . '[/b]' . "\n";
-	$o .= $r[0]['body'] . "\n";
+	$o .= $r[0]['body'] . "\n" ;
+
+	$o .= (($r[0]['plink']) ? '[url=' . $r[0]['plink'] . ']' . t('link') . '[/url]' . "\n" : '');
 
 	echo $o;
 	killme();  
