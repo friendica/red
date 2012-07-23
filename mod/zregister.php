eb4d5f4009a0443fe9e8b68eaa6ed1096a719a3c
friendica <info@friendica.com>
2012-07-22 19:37:15 -0700

    registration page form validation

--- mod/zregister.php
+++ mod/zregister.php
@@ -12,9 +12,9 @@ function zregister_init(&$a) {
 		$email = $_REQUEST['email'];
 
 		if(! allowed_email($email))
-			$result['message'] = t('Your email domain is not among those allowed on this site.');
+			$result['message'] = t('Your email domain is not among those allowed on this site');
 		if((! valid_email($email)) || (! validate_email($email)))
-			$result['message'] .= t('Not a valid email address.') . EOL;
+			$result['message'] .= t('Not a valid email address') . EOL;
 		if($result['message'])
 			$result['error'] = true;
 
@@ -23,8 +23,54 @@ function zregister_init(&$a) {
 		killme();		
 	}
 
+	$pw1 = t("Password too short");
+	$pw2 = t("Passwords do not match");
 
+	$a->page['htmlhead'] .= <<< EOT
+<script>
+	function zFormError(elm,x) {
+		if(x) {
+			$(elm).addClass("zform-error");
+			$(elm).removeClass("zform-ok");
+		}
+		else {
+			$(elm).addClass("zform-ok");
+			$(elm).removeClass("zform-error");
+		}											
+	}
+	$(document).ready(function() {
+		$("#zregister-email").blur(function() {
+			var zreg_email = $("#zregister-email").val();
+			$.get("zregister/email_check.json?f=&email=" + encodeURIComponent(zreg_email),function(data) {
+				$("#zregister-email-feedback").html(data.message);
+				zFormError("#zregister-email-feedback",data.error);
+			});
+		});
+		$("#zregister-password").blur(function() {
+			if(($("#zregister-password").val()).length < 6 ) {
+				$("#zregister-password-feedback").html("$pw1");
+				zFormError("#zregister-password-feedback",true);
+			}
+			else {
+				$("#zregister-password-feedback").html("");
+				zFormError("#zregister-password-feedback",false);
+			}
+		});
+		$("#zregister-password2").blur(function() {
+			if($("#zregister-password").val() != $("#zregister-password2").val()) {
+				$("#zregister-password2-feedback").html("$pw2");
+				zFormError("#zregister-password2-feedback",true);
+			}
+			else {
+				$("#zregister-password2-feedback").html("");
+				zFormError("#zregister-password2-feedback",false);
+			}
+		});
+	});
 
+</script>
+
+EOT;
 
 }
 
