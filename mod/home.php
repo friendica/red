2c9db93d8f2cd678ffeb992af2cf58d8ecc19465
mike <mike@zeus.(none)>
2012-10-22 19:35:05 +1100

    slight optimisation

--- mod/home.php
+++ mod/home.php
@@ -8,8 +8,8 @@ function home_init(&$a) {
 
 	$channel = $a->get_channel();
 
-	if(local_user() && $channel && $channel['channel_address'])
-		goaway( $a->get_baseurl() . '/profile/' . $channel['channel_address']);
+	if(local_user() && $channel && $channel['channel_profile'])
+		goaway( $channel['channel_profile']);
 
 }}
 
