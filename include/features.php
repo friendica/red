5b1a15704b81af507bddb623fefa3a9f416050b4
friendica <info@friendica.com>
2012-10-26 15:31:10 -0700

    feature management

---
+++ include/features.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Features management
+ */
+
+
+function feature_enabled($uid,$feature) {
+	$x = get_pconfig($uid,'feature',$feature);
+	$arr = array('uid' => $uid, 'feature' => $feature, 'enabled' => $x);
+	call_hooks('feature_enabled',$arr);
+	return($arr['enabled']);
+}
+
+function get_features() {
+
+$arr = array(
+
+'multi_delete' => t('Multiple Deletion'),
+'expire'       => t('Content Expiration'),
+'commtag'      => t('Community Tagging'),
+'categories'   => t('Post Categories'),
+'filing'       => t('Saved Folders'),
+'archives'     => t('Archives'),
+
+
+
+);
+
+	call_hooks('get_features',$arr);
+	return $arr;
+}
