a7f08173640326a97ae798dd1f6c96c1c8d26376
friendica <info@friendica.com>
2012-11-16 14:12:01 -0800

    site/friendica is now site/siteinfo

--- mod/siteinfo.php
+++ mod/siteinfo.php
@@ -1,6 +1,7 @@
 <?php
 
 function friendica_init(&$a) {
+
 	if ($a->argv[1]=="json"){
 		$register_policy = Array('REGISTER_CLOSED', 'REGISTER_APPROVE', 'REGISTER_OPEN');
 
@@ -44,18 +45,23 @@ function friendica_init(&$a) {
 
 
 
-function friendica_content(&$a) {
+function siteinfo_content(&$a) {
+
+	// FIXME Gak - this needs to be a template.
 
 	$o = '';
-	$o .= '<h3>Friendica</h3>';
+	$o .= '<h3>' . t('Red') . '</h3>';
 
 
 	$o .= '<p></p><p>';
 
-	$o .= t('This is Friendica, version') . ' ' . FRIENDICA_VERSION . ' ';
-	$o .= t('running at web location') . ' ' . z_root() . '</p><p>';
+	$o .= t('This is Red - another decentralized, distributed communications project by the folks at Friendica.');
+	if(! get_config('system','hidden_version_siteinfo'))
+		$o .= '</p><p>' . sprintf( t('Version %s'), FRIENDICA_VERSION ) . '</p><p>';
+	$o .= '</p><p>';
+	$o .= t('Running at web location') . ' ' . z_root() . '</p><p>';
 
-	$o .= t('Please visit <a href="http://friendica.com">Friendica.com</a> to learn more about the Friendica project.') . '</p><p>';	
+	$o .= t('Please visit <a href="http://friendica.com">Friendica.com</a> to learn more about the Friendica and/or Red project.') . '</p><p>';	
 
 	$o .= t('Bug reports and issues: please visit') . ' ' . '<a href="http://bugs.friendica.com">Bugs.Friendica.com</a></p><p>';
 	$o .= t('Suggestions, praise, donations, etc. - please email "Info" at Friendica - dot com') . '</p>';
