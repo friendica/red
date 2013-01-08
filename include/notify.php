948644bd46036202a098416eee546aab5d0bf978
friendica <info@friendica.com>
2013-01-08 13:38:07 -0800

    minor notification tweaks

--- include/notify.php
+++ include/notify.php
@@ -28,7 +28,7 @@ function format_notification($item) {
 	// convert this logic into a json array just like the system notifications
 
 	return array(
-		'notify_link' => z_root() . '/notify/view_item/' . $item['id'], 
+		'notify_link' => z_root() . '/display/' . $item['llink'], 
 		'name' => $item['author']['xchan_name'],
 		'url' => $item['author']['xchan_url'],
 		'photo' => $item['author']['xchan_photo_s'],
