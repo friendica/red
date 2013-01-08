18d5131fc16c2a49fa5ee5204b36ded7104894b4
friendica <info@friendica.com>
2013-01-08 14:24:32 -0800

    this should fix pcss

--- mod/view.php
+++ mod/view.php
@@ -4,14 +4,12 @@
  */
  
 function view_init($a){
+
 	header("Content-Type: text/css");
 		
-	if ($a->argc == 4){
-		$theme = $a->argv[2];
-		$THEMEPATH = "view/theme/$theme";
-		if(file_exists("view/theme/$theme/php/style.php"))
-			require_once("view/theme/$theme/php/style.php");
-	}
-	
+	$theme = argv(2);
+	$THEMEPATH = "view/theme/$theme";
+	if(file_exists("view/theme/$theme/php/style.php"))
+		require_once("view/theme/$theme/php/style.php");	
 	killme();
 }
