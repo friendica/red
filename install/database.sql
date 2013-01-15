482e0659c4f9525ba26a3b9344f39f8694e09304
friendica <info@friendica.com>
2013-01-15 14:34:14 -0800

    menu structures for linking your pages to other pages or whatever

--- install/database.sql
+++ install/database.sql
@@ -509,7 +509,7 @@ CREATE TABLE IF NOT EXISTS `item_id` (
   KEY `sid` (`sid`),
   KEY `service` (`service`),
   KEY `iid` (`iid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `mail` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -541,6 +541,30 @@ CREATE TABLE IF NOT EXISTS `manage` (
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`),
   KEY `mid` (`mid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `menu_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_channel_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `menu_desc` char(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`menu_id`),
+  KEY `menu_channel_id` (`menu_channel_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `menu_item` (
+  `mitem_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `mitem_link` char(255) NOT NULL DEFAULT '',
+  `mitem_desc` char(255) NOT NULL DEFAULT '',
+  `allow_cid` mediumtext NOT NULL,
+  `allow_gid` mediumtext NOT NULL,
+  `deny_cid` mediumtext NOT NULL,
+  `deny_gid` mediumtext NOT NULL,
+  `mitem_channel_id` int(10) unsigned NOT NULL,
+  `mitem_menu_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `mitem_order` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`mitem_id`),
+  KEY `mitem_channel_id` (`mitem_channel_id`),
+  KEY `mitem_menu_id` (`mitem_menu_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `notify` (
--- install/update.php
+++ install/update.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'UPDATE_VERSION' , 1016 );
+define( 'UPDATE_VERSION' , 1017 );
 
 /**
  *
@@ -224,6 +224,39 @@ function update_r1015() {
 ADD `channel_w_pages` INT UNSIGNED NOT NULL DEFAULT '128'");
 
 	$r2 = q("ALTER TABLE `channel` ADD INDEX ( `channel_r_pages` ) , ADD INDEX ( `channel_w_pages` ) ");
+
+	if($r && $r2)
+		return UPDATE_SUCCESS;
+	return UPDATE_FAILED;
+}
+
+
+function update_r1016() {
+
+	$r = q("CREATE TABLE IF NOT EXISTS `menu` (
+  `menu_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_channel_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `menu_desc` char(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`menu_id`),
+  KEY `menu_channel_id` (`menu_channel_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ");
+
+	$r2 = q("CREATE TABLE IF NOT EXISTS `menu_item` (
+  `mitem_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `mitem_link` char(255) NOT NULL DEFAULT '',
+  `mitem_desc` char(255) NOT NULL DEFAULT '',
+  `allow_cid` mediumtext NOT NULL,
+  `allow_gid` mediumtext NOT NULL,
+  `deny_cid` mediumtext NOT NULL,
+  `deny_gid` mediumtext NOT NULL,
+  `mitem_channel_id` int(10) unsigned NOT NULL,
+  `mitem_menu_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `mitem_order` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`mitem_id`),
+  KEY `mitem_channel_id` (`mitem_channel_id`),
+  KEY `mitem_menu_id` (`mitem_menu_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ");
+
 
 	if($r && $r2)
 		return UPDATE_SUCCESS;
