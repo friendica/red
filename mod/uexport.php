4734e2ea8731ce51e44aac4065430445d3146845
friendica <info@friendica.com>
2013-01-16 22:41:39 -0800

    export should always "save to file" by default

--- mod/uexport.php
+++ mod/uexport.php
@@ -4,12 +4,22 @@ function uexport_init(&$a) {
 	if(! local_user())
 		killme();
 
+	$channel = $a->get_channel();
+
 	require_once('include/identity.php');
 
-	if(argc() > 1 && argv(1) === 'basic')
-		json_return_and_die(identity_basic_export(local_user()));
+	header('content-type: application/octet_stream');
+	header('content-disposition: attachment; filename="' . $channel['channel_address'] . '.json"' );
 
-	if(argc() > 1 && argv(1) === 'complete')
-		json_return_and_die('not yet implemented');
+
+	if(argc() > 1 && argv(1) === 'basic') {
+		echo json_encode(identity_basic_export(local_user()));
+		killme();
+	}
+
+	if(argc() > 1 && argv(1) === 'complete') {
+		echo json_encode('not yet implemented');
+		killme();
+	}
 	
 }
