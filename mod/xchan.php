c42dbebd7624c78df9e2a063feda3e3fc97440b7
friendica <info@friendica.com>
2013-02-10 20:17:55 -0800

    xchan lookup tool - provide the first few chars of an xchan and find out who it is

---
+++ mod/xchan.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function xchan_content(&$a) {
+
+
+	$o .= '<h3>Xchan Lookup</h3>';
+
+	$o .= '<form action="xchan" method="get">';
+	$o .= 'Lookup xchan beginning with: <input type="text" style="width: 250px;" name="addr" value="' . $_GET['addr'] .'" />';
+	$o .= '<input type="submit" name="submit" value="Submit" /></form>'; 
+
+	$o .= '<br /><br />';
+
+	if(x($_GET,'addr')) {
+		$addr = trim($_GET['addr']);
+		$r = q("select xchan_name from xchan where xchan_hash like '%s%%'",
+			dbesc(addr)
+		);
+		if($r) {
+			foreach($r as $rr)
+				$o .= $rr['xchan_name'] . EOL;
+		}
+		else
+			notice( t('Not found.') . EOL);
+	}	
+	return $o;
+}
