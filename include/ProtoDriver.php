6e6a83194a47a49d4ed9a3daf646e970322aec05
friendica <info@friendica.com>
2013-02-18 16:27:54 -0800

    nothing to see, keep moving

---
+++ include/ProtoDriver.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Abstraction class for dealing with alternate networks (which of course do not exist, hence the abstraction)
+ */
+
+
+abstract class ProtoDriver {
+	abstract protected function discover($channel,$location);
+	abstract protected function deliver($item,$channel,$recipients);
+	abstract protected function collect($channel,$connection);
+	abstract protected function change_permissions($permissions,$channel,$recipient);
+	abstract protected function acknowledge_permissions($permissions,$channel,$recipient);
+	abstract protected function deliver_private($item,$channel,$recipients);
+	abstract protected function collect_private($channel,$connection);
+
+}
+
+class ZotDriver extends ProtoDriver {
+
+	protected function discover($channel,$location) {
+
+	}
+	protected function deliver($item,$channel,$recipients) {
+
+	}
+	protected function collect($channel,$connection) {
+
+	}
+	protected function change_permissions($permissions,$channel,$recipient) {
+
+	}
+	protected function acknowledge_permissions($permissions,$channel,$recipient) {
+
+	}
+	protected function deliver_private($item,$channel,$recipients) {
+
+	}
+	protected function collect_private($channel,$connection) {
+
+	}
+
+}
