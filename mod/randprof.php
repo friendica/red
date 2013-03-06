5054b6e363a9e77886f01c639b909f457aaa5819
friendica <info@friendica.com>
2013-03-06 00:25:37 -0800

    add a fixme

--- mod/randprof.php
+++ mod/randprof.php
@@ -6,5 +6,6 @@ function randprof_init(&$a) {
 	$x = random_profile();
 	if($x)
 		goaway(chanlink_url($x));
+	// FIXME this doesn't work at the moment as a fallback
 	goaway($a->get_baseurl() . '/profile');
 }
