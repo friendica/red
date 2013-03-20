499083d5d4b2e2a09003148e3da787c632871934
Thomas Willingham <founder@kakste.com>
2013-03-20 23:43:02 +0000

    Revert randprof - that was stupid.  If you have a local channel, you also have it's xchan.

--- mod/randprof.php
+++ mod/randprof.php
@@ -1,24 +1,11 @@
 <?php
 
-function randprof_fallback() {
-        $r = q("select channel_address from channel where channel_r_stream = 1 order by rand() limit 1");
-if($r)
-        return $r[0]['channel_address'];
-return '';
-}
 
 function randprof_init(&$a) {
 	require_once('include/Contact.php');
 	$x = random_profile();
 	if($x)
 		goaway(chanlink_url($x));
-	// Nothing there, so try a local, public channel instead
-	else $x = randprof_fallback();
-	  if($x) {
-	  $goaway = (z_root() . '/channel/' . $x);
-	    goaway(chanlink_url($goaway));}
-	
-	// If we STILL haven't got anything, send them to their own profile, or the front page
-	
-	  goaway($a->get_baseurl());
+	// FIXME this doesn't work at the moment as a fallback
+	goaway($a->get_baseurl() . '/profile');
 }
