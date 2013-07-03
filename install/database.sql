429ebabfd98240fe7d69ba81879ee35769af305f
friendica <info@friendica.com>
2013-07-03 04:28:36 -0700

    any questions regarding this checkin will be ignored

--- install/database.sql
+++ install/database.sql
@@ -599,6 +599,23 @@ CREATE TABLE IF NOT EXISTS `notify` (
   KEY `otype` (`otype`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `obj` (
+  `obj_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `obj_page` char(64) NOT NULL DEFAULT '',
+  `obj_verb` char(255) NOT NULL DEFAULT '',
+  `obj_type` int(10) unsigned NOT NULL DEFAULT '0',
+  `obj_obj` char(255) NOT NULL DEFAULT '',
+  `obj_channel` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`obj_id`),
+  KEY `obj_verb` (`obj_verb`),
+  KEY `obj_page` (`obj_page`),
+  KEY `obj_type` (`obj_type`),
+  KEY `obj_channel` (`obj_channel`),
+  KEY `obj_obj` (`obj_obj`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
 CREATE TABLE IF NOT EXISTS `outq` (
   `outq_hash` char(255) NOT NULL,
   `outq_account` int(10) unsigned NOT NULL DEFAULT '0',
--- install/update.php
+++ install/update.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'UPDATE_VERSION' , 1048 );
+define( 'UPDATE_VERSION' , 1049 );
 
 /**
  *
@@ -581,3 +581,23 @@ ADD INDEX ( `xprof_age` ) ");
 	return UPDATE_FAILED;
 }
 
+function update_r1048() {
+	$r = q("CREATE TABLE IF NOT EXISTS `obj` (
+  `obj_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `obj_page` char(64) NOT NULL DEFAULT '',
+  `obj_verb` char(255) NOT NULL DEFAULT '',
+  `obj_type` int(10) unsigned NOT NULL DEFAULT '0',
+  `obj_obj` char(255) NOT NULL DEFAULT '',
+  `obj_channel` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`obj_id`),
+  KEY `obj_verb` (`obj_verb`),
+  KEY `obj_page` (`obj_page`),
+  KEY `obj_type` (`obj_type`),
+  KEY `obj_channel` (`obj_channel`),
+  KEY `obj_obj` (`obj_obj`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ");
+
+	if($r)
+		return UPDATE_SUCCESS;
+	return UPDATE_FAILED;
+}
