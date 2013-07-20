2f0c853ba0f619d26a63290cb55926fce99c6ed4
friendica <info@friendica.com>
2013-07-20 05:20:14 -0700

    clone sync: extra param to zot_build_packet

--- include/settings.php
+++ include/settings.php
@@ -78,7 +78,7 @@ function build_sync_packet($uid = 0, $packet = null) {
 
 	foreach($synchubs as $hub) {
 		$hash = random_string();
-		$n = zot_build_packet($channel,'notify',$env_recips,$hub['hubloc_sitekey'],null,$hash);
+		$n = zot_build_packet($channel,'notify',$env_recips,$hub['hubloc_sitekey'],$hash);
 		q("insert into outq ( outq_hash, outq_account, outq_channel, outq_posturl, outq_async, outq_created, outq_updated, outq_notify, outq_msg ) values ( '%s', %d, %d, '%s', %d, '%s', '%s', '%s', '%s' )",
 			dbesc($hash),
 			intval($channel['channel_account']),
