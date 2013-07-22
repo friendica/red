9b8e3e41f1b582f3eb0ff70edd0e22b7fb9b4e89
friendica <info@friendica.com>
2013-07-21 20:19:05 -0700

    get rid of ugly article wrapper

--- view/php/default.php
+++ view/php/default.php
@@ -6,7 +6,6 @@
   <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
 </head>
 <body>
-	<article>
 	<?php if(x($page,'nav')) echo $page['nav']; ?>
 	<aside><?php if(x($page,'aside')) echo $page['aside']; ?></aside>
 	<section><?php if(x($page,'content')) echo $page['content']; ?>
@@ -15,7 +14,6 @@
 	</section>
 	<right_aside><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></right_aside>
 	<footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>
-	</article>
 </body>
 </html>
 
