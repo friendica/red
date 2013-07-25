30da63287888a754c864fa80b3803a13f68c0575
friendica <info@friendica.com>
2013-07-24 17:03:47 -0700

    get rid of legacy (unused) encryption stuff.

--- include/crypto.php
+++ include/crypto.php
@@ -1,8 +1,5 @@
 <?php /** @file */
 
-require_once('library/ASNValue.class.php');
-require_once('library/asn1.php');
-
 function rsa_sign($data,$key,$alg = 'sha256') {
 	if(! $key)
 		return 'no key';
@@ -20,177 +17,6 @@ function rsa_verify($data,$sig,$key,$alg = 'sha256') {
 	return $verify;
 }
 
-
-function DerToPem($Der, $Private=false)
-{
-    //Encode:
-    $Der = base64_encode($Der);
-    //Split lines:
-    $lines = str_split($Der, 65);
-    $body = implode("\n", $lines);
-    //Get title:
-    $title = $Private? 'RSA PRIVATE KEY' : 'PUBLIC KEY';
-    //Add wrapping:
-    $result = "-----BEGIN {$title}-----\n";
-    $result .= $body . "\n";
-    $result .= "-----END {$title}-----\n";
- 
-    return $result;
-}
-
-function DerToRsa($Der)
-{
-    //Encode:
-    $Der = base64_encode($Der);
-    //Split lines:
-    $lines = str_split($Der, 64);
-    $body = implode("\n", $lines);
-    //Get title:
-    $title = 'RSA PUBLIC KEY';
-    //Add wrapping:
-    $result = "-----BEGIN {$title}-----\n";
-    $result .= $body . "\n";
-    $result .= "-----END {$title}-----\n";
- 
-    return $result;
-}
-
-
-function pkcs8_encode($Modulus,$PublicExponent) {
-	//Encode key sequence
-	$modulus = new ASNValue(ASNValue::TAG_INTEGER);
-	$modulus->SetIntBuffer($Modulus);
-	$publicExponent = new ASNValue(ASNValue::TAG_INTEGER);
-	$publicExponent->SetIntBuffer($PublicExponent);
-	$keySequenceItems = array($modulus, $publicExponent);
-	$keySequence = new ASNValue(ASNValue::TAG_SEQUENCE);
-	$keySequence->SetSequence($keySequenceItems);
-	//Encode bit string
-	$bitStringValue = $keySequence->Encode();
-	$bitStringValue = chr(0x00) . $bitStringValue; //Add unused bits byte
-	$bitString = new ASNValue(ASNValue::TAG_BITSTRING);
-	$bitString->Value = $bitStringValue;
-	//Encode body
-	$bodyValue = "\x30\x0d\x06\x09\x2a\x86\x48\x86\xf7\x0d\x01\x01\x01\x05\x00" . $bitString->Encode();
-	$body = new ASNValue(ASNValue::TAG_SEQUENCE);
-	$body->Value = $bodyValue;
-	//Get DER encoded public key:
-	$PublicDER = $body->Encode();
-	return $PublicDER;
-}
-
-
-function pkcs1_encode($Modulus,$PublicExponent) {
-	//Encode key sequence
-	$modulus = new ASNValue(ASNValue::TAG_INTEGER);
-	$modulus->SetIntBuffer($Modulus);
-	$publicExponent = new ASNValue(ASNValue::TAG_INTEGER);
-	$publicExponent->SetIntBuffer($PublicExponent);
-	$keySequenceItems = array($modulus, $publicExponent);
-	$keySequence = new ASNValue(ASNValue::TAG_SEQUENCE);
-	$keySequence->SetSequence($keySequenceItems);
-	//Encode bit string
-	$bitStringValue = $keySequence->Encode();
-	return $bitStringValue;
-}
-
-
-function metopem($m,$e) {
-	$der = pkcs8_encode($m,$e);
-	$key = DerToPem($der,false);
-	return $key;
-}	
-
-
-function pubrsatome($key,&$m,&$e) {
-	require_once('library/asn1.php');
-
-	$lines = explode("\n",$key);
-	unset($lines[0]);
-	unset($lines[count($lines)]);
-	$x = base64_decode(implode('',$lines));
-
-	$r = ASN_BASE::parseASNString($x);
-
-	$m = base64url_decode($r[0]->asnData[0]->asnData);
-	$e = base64url_decode($r[0]->asnData[1]->asnData);
-}
-
-
-function rsatopem($key) {
-	pubrsatome($key,$m,$e);
-	return(metopem($m,$e));
-}
-
-function pemtorsa($key) {
-	pemtome($key,$m,$e);
-	return(metorsa($m,$e));
-}
-
-function pemtome($key,&$m,&$e) {
-	$lines = explode("\n",$key);
-	unset($lines[0]);
-	unset($lines[count($lines)]);
-	$x = base64_decode(implode('',$lines));
-
-	$r = ASN_BASE::parseASNString($x);
-
-	$m = base64url_decode($r[0]->asnData[1]->asnData[0]->asnData[0]->asnData);
-	$e = base64url_decode($r[0]->asnData[1]->asnData[0]->asnData[1]->asnData);
-}
-
-function metorsa($m,$e) {
-	$der = pkcs1_encode($m,$e);
-	$key = DerToRsa($der);
-	return $key;
-}	
-
-function salmon_key($pubkey) {
-	pemtome($pubkey,$m,$e);
-	return 'RSA' . '.' . base64url_encode($m,true) . '.' . base64url_encode($e,true) ;
-}
-
-
-
-if(! function_exists('aes_decrypt')) {
-function aes_decrypt($val,$ky)
-{
-    $key="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-    for($a=0;$a<strlen($ky);$a++)
-      $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a]));
-    $mode = MCRYPT_MODE_ECB;
-    $enc = MCRYPT_RIJNDAEL_128;
-    $dec = @mcrypt_decrypt($enc, $key, $val, $mode, @mcrypt_create_iv( @mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM ) );
-    return rtrim($dec,(( ord(substr($dec,strlen($dec)-1,1))>=0 and ord(substr($dec, strlen($dec)-1,1))<=16)? chr(ord( substr($dec,strlen($dec)-1,1))):null));
-}}
-
-
-if(! function_exists('aes_encrypt')) {
-function aes_encrypt($val,$ky)
-{
-    $key="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-    for($a=0;$a<strlen($ky);$a++)
-      $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a]));
-    $mode=MCRYPT_MODE_ECB;
-    $enc=MCRYPT_RIJNDAEL_128;
-    $val=str_pad($val, (16*(floor(strlen($val) / 16)+(strlen($val) % 16==0?2:1))), chr(16-(strlen($val) % 16)));
-    return mcrypt_encrypt($enc, $key, $val, $mode, mcrypt_create_iv( mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM));
-}} 
-
-
-function pkcs5_pad ($text, $blocksize)
-{
-    $pad = $blocksize - (strlen($text) % $blocksize);
-    return $text . str_repeat(chr($pad), $pad);
-}
-
-function pkcs5_unpad($text)
-{
-    $pad = ord($text{strlen($text)-1});
-    if ($pad > strlen($text)) return false;
-    if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) return false;
-    return substr($text, 0, -1 * $pad);
-} 
 
 function AES256CBC_encrypt($data,$key,$iv) {
 	return mcrypt_encrypt(
@@ -216,6 +42,7 @@ function aes_encapsulate($data,$pubkey) {
 	$key = random_string(32,RANDOM_STRING_TEXT);
 	$iv  = random_string(16,RANDOM_STRING_TEXT);
 	$result['data'] = base64url_encode(AES256CBC_encrypt($data,$key,$iv),true);
+	// log the offending call so we can track it down
 	if(! openssl_public_encrypt($key,$k,$pubkey)) {
 		$x = debug_backtrace();
 		logger('aes_encapsulate: RSA failed. ' . print_r($x[0],true));
