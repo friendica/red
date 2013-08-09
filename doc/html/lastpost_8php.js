7e97b358fda050238654fe85bf40196b69623eed
friendica <info@friendica.com>
2013-08-09 01:24:50 -0700

    add a few missing doc pages

---
+++ doc/html/lastpost_8php.js
@@ -0,0 +1,6 @@
+var lastpost_8php =
+[
+    [ "lastpost_aside", "lastpost_8php.html#a6108289ef2a767495c7c85a24f364983", null ],
+    [ "lastpost_content", "lastpost_8php.html#a82f14cce5d3cfe27f40bdbd2c679d493", null ],
+    [ "lastpost_init", "lastpost_8php.html#ab4c6ebd25736af678e64d55ce508ce8c", null ]
+];
