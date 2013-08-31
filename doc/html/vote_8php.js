625dcb911a01983f4767a55567a0374d6045093e
friendica <info@friendica.com>
2013-08-31 04:22:40 -0700

    doc updates

---
+++ doc/html/vote_8php.js
@@ -0,0 +1,6 @@
+var vote_8php =
+[
+    [ "vote_content", "vote_8php.html#a6aa67489bf458ca5e3206e46dac68596", null ],
+    [ "vote_init", "vote_8php.html#ae0c6610f40afbbc1f4fe6494c51fbab2", null ],
+    [ "vote_post", "vote_8php.html#a57a9516ee1b923b224e66dcc47377fb2", null ]
+];
