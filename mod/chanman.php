135c1887328f7c2e03d03c0c41d6ab4773996519
friendica <info@friendica.com>
2013-09-08 19:12:23 -0700

    description of the chanman module and what it will need to do.

---
+++ mod/chanman.php
@@ -0,0 +1,29 @@
+<?php /** @file */
+
+
+/**
+	Placeholder file at present. This is going to involve a bit of work.
+
+	This file will deal with the deletion of channels and management of hublocs.
+
+	We need to provide the following functionality:
+
+	- Delete my account and all channels from the entire network
+
+	- Delete my account and all channels from this server
+
+	- Delete a channel from the entire network
+
+	- Delete a channel from this server
+
+	- List all hub locations for this channel
+
+	- Remove this/some hub location from this channel
+
+	- promote this/some hub location to primary
+
+	- Remove hub location 'xyz' from this channel, (this should possibly only be allowed if that hub has been down for a period of time)
+
+	- Some of these actions should probably require email verification
+
+*/
