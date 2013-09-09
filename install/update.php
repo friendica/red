19165aef1e5ad3eefa139ea54cf56e37daad4d4c
Michael Meer <michael@meer.name>
2013-09-09 15:58:20 +0200

    increment number

--- install/update.php
+++ install/update.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'UPDATE_VERSION' , 1068 );
+define( 'UPDATE_VERSION' , 1069 );
 
 /**
  *
@@ -774,7 +774,16 @@ ADD INDEX ( `site_access` )");
 		return UPDATE_SUCCESS;
 	return UPDATE_FAILED;
 }
+function update_r1067() {
+	$r = q("ALTER TABLE `updates` DROP PRIMARY KEY , ADD `ud_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST, ADD INDEX ( `ud_hash` ) ");
+	if($r)
+		return UPDATE_SUCCESS;
+	return UPDATE_FAILED;
+}
 
-function update_r1067(){
+function update_r1068(){
 	$r = q("ALTER TABLE `hubloc` ADD `hubloc_status` INT NOT NULL DEFAULT '0' AFTER `hubloc_flags` , ADD INDEX ( `hubloc_status` )");
+	if($r)
+		return UPDATE_SUCCESS;
+	return UPDATE_FAILED
 }
