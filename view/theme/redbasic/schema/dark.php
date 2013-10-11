033b1ed430962195cdb329b5bc532f31bf8c4af5
Thomas Willingham <founder@kakste.com>
2013-10-11 17:36:02 +0100

    Dark schema, and associated fixes.

---
+++ view/theme/redbasic/schema/dark.php
@@ -0,0 +1,10 @@
+<?php
+	
+	if (! $nav_colour)
+		$nav_colour = "black";
+	if (! $bgcolour)
+		$bgcolour = '#010101';
+	if (! $item_colour)
+		$item_colour = '#111';
+	if (! $font_colour)
+		$font_colour = '#fff';
