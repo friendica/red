6f2163a08ec6b48a113e768c8fc0a94f8dd0bde1
Thomas Willingham <founder@kakste.com>
2013-11-09 18:13:08 +0000

    Add a safesearch toggle

---
+++ mod/toggle_safesearch.php
@@ -0,0 +1,22 @@
+<?php
+
+function toggle_safesearch_init(&$a) {
+
+$observer = get_observer_hash();
+
+if($observer) 
+	$safe_mode = get_xconfig($observer,'directory','safe_mode');
+	
+if($safe_mode == 0)
+	set_xconfig($observer,'directory','safe_mode', '1');
+if($safe_mode == 1)
+	set_xconfig($observer,'directory','safe_mode', '0');
+
+if(isset($_GET['address']))
+	$address = $_GET['address'];
+else
+	$address = z_root();
+
+	goaway($address);
+}
+	
