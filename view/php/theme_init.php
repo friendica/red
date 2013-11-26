95824642a643ccee269f7bf1a5ac570bc966a2e2
friendica <info@friendica.com>
2013-11-25 19:07:58 -0800

    make font-asewome optional. Also include jquery.browser in js/main.js so we can turn off the migration toolkit.

--- view/php/theme_init.php
+++ view/php/theme_init.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
 require_once('include/plugin.php');
-
 
 head_add_css('library/fancybox/jquery.fancybox-1.3.4.css');
 head_add_css('library/tiptip/tipTip.css');
@@ -11,10 +8,10 @@ head_add_css('library/jgrowl/jquery.jgrowl.css');
 head_add_css('library/jslider/css/jslider.css');
 head_add_css('library/prettyphoto/css/prettyPhoto.css');
 head_add_css('library/colorbox/colorbox.css');
-head_add_css('library/font_awesome/css/font-awesome.min.css');
+// head_add_css('library/font_awesome/css/font-awesome.min.css');
 
 head_add_js('js/jquery.js');
-head_add_js('js/jquery-migrate-1.1.1.js');
+// head_add_js('js/jquery-migrate-1.1.1.js');
 head_add_js('js/spin.js');
 head_add_js('js/jquery.spin.js');
 head_add_js('js/jquery.textinputs.js');
---
+++ view/theme/redbasic/php/theme_init.php
@@ -0,0 +1,5 @@
+<?php
+
+head_add_css('library/font_awesome/css/font-awesome.min.css');
+require_once('view/php/theme_init.php');
+
