38df3f640458ddb3d6ddd4055b275c59085f14d0
friendica <info@friendica.com>
2013-12-22 16:53:26 -0800

    make xref work

--- mod/xref.php
+++ mod/xref.php
@@ -1,20 +1,20 @@
 <?php
 
-	function ref_init(&$a) {
-		// Sets a referral URL using an xchan directly
-		// Link format: example.com/xref/[xchan]/[TargetURL]
-		// Target URL is optional.
-		// Cookie lasts 24 hours to survive a browser restart.  Contains no personal
-		// information at all - just somebody else's xchan.
-			$referrer = argv(1);
-			$expire=time()+60*60*24;
-			$path = 'ref';
-			setcookie(ref, $referrer, $expire, "/"); 
-			$url = '';
+function xref_init(&$a) {
+	// Sets a referral URL using an xchan directly
+	// Link format: example.com/xref/[xchan]/[TargetURL]
+	// Target URL is optional.
+	// Cookie lasts 24 hours to survive a browser restart.  Contains no personal
+	// information at all - just somebody else's xchan.
+	$referrer = argv(1);
+	$expire=time()+60*60*24;
+	$path = 'xref';
+	setcookie($path, $referrer, $expire, "/"); 
+	$url = '';
 
-		if (argc() > 2)
-			$url = argv(2);
+	if (argc() > 2)
+		$url = argv(2);
 			
-		goaway (z_root() . '/' . $url);
+	goaway (z_root() . '/' . $url);
 			
 }
