c0f6d7319524f8fb8bac764d6e2f306335d2266b
zottel <github@zottel.net>
2014-01-09 16:02:40 +0100

    save channel/<channel>/?mid=... plinks with the items

--- include/items.php
+++ include/items.php
@@ -1602,8 +1602,15 @@ function item_store($arr,$allow_exec = false) {
 
 	$arr['llink'] = z_root() . '/display/' . $arr['mid'];
 
-	if(! $arr['plink'])
-		$arr['plink'] = $arr['llink'];
+	if(! $arr['plink']) {
+		if (local_user()){
+			$channel = get_app()->get_channel();
+			$arr['plink'] = z_root() . '/channel/' . $channel['channel_address'] . '/?mid=' . $arr['mid'];
+		} else {
+			// can this ever happen?
+			$arr['plink'] = $arr['llink'];
+		}
+	}
 
 	if($arr['parent_mid'] === $arr['mid']) {
 		$parent_id = 0;
