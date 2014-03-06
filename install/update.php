2a1729e6718a6a09fb322d8e115b5d47235ce809
friendica <info@friendica.com>
2014-03-06 01:34:39 -0800

    typo

--- install/update.php
+++ install/update.php
@@ -1155,7 +1155,7 @@ function update_r1102() {
 	$r = q("update abook set abook_flags = (abook_flags - %d)
 		where ( abook_flags & %d)",
 		intval(ABOOK_FLAG_UNCONNECTED),
-		intval(ABOOK_FLAG_UNCONNECTED),
+		intval(ABOOK_FLAG_UNCONNECTED)
 	);
 	return UPDATE_SUCCESS;
 }
