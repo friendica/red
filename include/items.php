78d7425a1d9de91343c60be1d0f0a467a674d84d
friendica <info@friendica.com>
2014-03-31 17:21:16 -0700

    wrong var name in add_source_route

--- include/items.php
+++ include/items.php
@@ -155,9 +155,9 @@ function can_comment_on_post($observer_xchan,$item) {
 
 
 function add_source_route($iid,$hash) {
-	logger('add_source_route ' . $iid . ' ' . $hash, LOGGER_DEBUG);
+//	logger('add_source_route ' . $iid . ' ' . $hash, LOGGER_DEBUG);
 
-	if((! $iid) || (! $route))
+	if((! $iid) || (! $hash))
 		return;
 	$r = q("select route from item where id = %d limit 1",
 		intval($iid)
