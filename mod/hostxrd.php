0a097b833750ad8941dba03e6821c13facebd597
friendica <info@friendica.com>
2014-04-02 14:56:20 -0700

    snakebite: bring back some of the legacy XML discovery interfaces, update as required.

--- mod/hostxrd.php
+++ mod/hostxrd.php
@@ -1,28 +1,16 @@
 <?php
 
-require_once('include/crypto.php');
-
 function hostxrd_init(&$a) {
 	header('Access-Control-Allow-Origin: *');
 	header("Content-type: text/xml");
-	$pubkey = get_config('system','site_pubkey');
-
-	if(! $pubkey) {
-		$res = new_keypair(1024);
-
-		set_config('system','site_prvkey', $res['prvkey']);
-		set_config('system','site_pubkey', $res['pubkey']);
-	}
 
 	$tpl = get_markup_template('xrd_host.tpl');
-	echo replace_macros($tpl, array(
+	$x = replace_macros(get_markup_template('xrd_host.tpl'), array(
 		'$zhost' => $a->get_hostname(),
-		'$zroot' => z_root(),
-		'$domain' => z_path(),
-		'$zot_post' => z_root() . '/post',
-		'$bigkey' => salmon_key(get_config('system','site_pubkey')),
+		'$zroot' => z_root()
 	));
-	session_write_close();
-	exit();
-
+	$arr = array('xrd' => $x);
+	call_hooks('hostxrd',$arr);
+	echo $arr['xrd'];
+	killme();
 }
--- view/tpl/xrd_host.tpl
+++ view/tpl/xrd_host.tpl
@@ -4,15 +4,8 @@
  
     <hm:Host>{{$zhost}}</hm:Host>
  
-    <Link rel='lrdd' template='{{$domain}}/xrd/?uri={uri}' />
-    <Link rel='acct-mgmt' href='{{$domain}}/amcd' />
-    <Link rel='http://services.mozilla.com/amcd/0.1' href='{{$domain}}/amcd' />
+    <Link rel='lrdd' template='{{$zroot}}/xrd/?uri={uri}' />
 	<Link rel="http://oexchange.org/spec/0.8/rel/resident-target" type="application/xrd+xml" 
-        href="{{$domain}}/oexchange/xrd" />
-
-    <Property xmlns:mk="http://salmon-protocol.org/ns/magic-key"
-        type="http://salmon-protocol.org/ns/magic-key"
-        mk:key_id="1">{{$bigkey}}</Property>
-
+        href="{{$zroot}}/oexchange/xrd" />
 
 </XRD>
