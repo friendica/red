a65bf5beee1f22dff17c28e7567fa0090269a17d
jeroenpraat <jeroenpraat@xs4all.nl>
2014-04-03 13:32:39 +0200

    Update and fix dark scheme
    Updated the dark scheme as far as possible. Some items do not have a string, so they can't be themed. A dark derived theme would probably better.

--- view/theme/redbasic/schema/dark.php
+++ view/theme/redbasic/schema/dark.php
@@ -1,10 +1,36 @@
 <?php
 	
-	if (! $nav_colour)
-		$nav_colour = "black";
+	if (! $nav_bg)
+		$nav_bg = "#000000";
+	if (! $nav_gradient_top)
+		$nav_gradient_top = "#000000";
+	if (! $nav_gradient_bottom)
+		$nav_gradient_bottom = "#000000";
+	if (! $nav_active_gradient_top)
+		$nav_active_gradient_top = "#333333";
+	if (! $nav_active_gradient_bottom)
+		$nav_active_gradient_bottom = "#111111";
+	if (! $nav_bd)
+		$nav_bd = "#111111";
+	if (! $nav_icon_colour)
+		$nav_icon_colour = "#999999";
+	if (! $nav_active_icon_colour)
+		$nav_active_icon_colour = "#ffffff";
+	if (! $link_colour)
+		$link_colour = "#ffffff";
+	if (! $banner_colour)
+		$banner_colour = "#999999";
+	if (! $search_background)
+		$search_background = "#999999";
 	if (! $bgcolour)
-		$bgcolour = '#010101';
+		$bgcolour = "#111111";
+	if (! $background_image)
+		$background_image ='';
 	if (! $item_colour)
-		$item_colour = '#111';
+		$item_colour = "#111111";
+	if (! $toolicon_colour)
+		$toolicon_colour = '#999999';
+	if (! $toolicon_activecolour)
+		$toolicon_activecolour = '#ffffff';
 	if (! $font_colour)
-		$font_colour = '#fff';
+		$font_colour = "#cccccc";
