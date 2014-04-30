31fd284b3c99595a11e3a1401409f5ca2a5d5cfd
friendica <info@friendica.com>
2014-04-29 18:28:05 -0700

    clean up the webpage list a bit

--- mod/webpages.php
+++ mod/webpages.php
@@ -15,44 +15,44 @@ function webpages_content(&$a) {
 
 	if((local_user()) && (argc() > 2) && (argv(2) === 'view')) {
 		$which = $channel['channel_address'];
-		$profile = argv(1);		
+		$profile = argv(1);
 	}
 
 	profile_load($a,$which,$profile);
 
 
-// Figure out who the page owner is.
-        $r = q("select channel_id from channel where channel_address = '%s'",
-                dbesc($which)
-                );
-               if($r) {
-                $owner = intval($r[0]['channel_id']);
+	// Figure out who the page owner is.
+	$r = q("select channel_id from channel where channel_address = '%s'",
+		dbesc($which)
+	);
+	if($r) {
+		$owner = intval($r[0]['channel_id']);
 	}
 
-// Get the observer, check their permissions
+	// Get the observer, check their permissions
 
-        $observer = $a->get_observer();
-        $ob_hash = (($observer) ? $observer['xchan_hash'] : '');
+	$observer = $a->get_observer();
+	$ob_hash = (($observer) ? $observer['xchan_hash'] : '');
 
-        $perms = get_all_perms($owner,$ob_hash);
+	$perms = get_all_perms($owner,$ob_hash);
 
-        if(! $perms['write_pages']) {
-                notice( t('Permission denied.') . EOL);
-                return;
-        }
+	if(! $perms['write_pages']) {
+		notice( t('Permission denied.') . EOL);
+		return;
+	}
 
 //		if(local_user() && local_user() == $owner) {
 //			$a->set_widget('design',design_tools());
 //		}
 
 
-		$mimetype = get_config('system','page_mimetype');
-		if(! $mimetype)
-			$mimetype = 'choose';
+	$mimetype = get_config('system','page_mimetype');
+	if(! $mimetype)
+		$mimetype = 'choose';
 
-		$layout = get_config('system','page_layout');
-		if(! $layout)
-			$layout = 'choose';
+	$layout = get_config('system','page_layout');
+	if(! $layout)
+		$layout = 'choose';
 
 
 // Create a status editor (for now - we'll need a WYSIWYG eventually) to create pages
@@ -61,43 +61,43 @@ function webpages_content(&$a) {
 	require_once('include/acl_selectors.php');
 
 
-		if(local_user() && local_user() == $a->profile_uid) {
-			$channel = $a->get_channel();  
-			$channel_acl = array(
-				'allow_cid' => $channel['channel_allow_cid'], 
-				'allow_gid' => $channel['channel_allow_gid'], 
-				'deny_cid'  => $channel['channel_deny_cid'], 
-				'deny_gid'  => $channel['channel_deny_gid']
-			); 
-		}
-		else
-			$channel_acl = array();
-
-		require_once('include/conversation.php');
-		$o = profile_tabs($a,true);
-
-		$o .= '<h2>' . t('Webpages') . '</h2>';
-
-		$x = array(
-			'webpage' => ITEM_WEBPAGE,
-			'is_owner' => true,
-			'nickname' => $a->profile['channel_address'],
-			'lockstate' => (($group || $cid || $channel['channel_allow_cid'] || $channel['channel_allow_gid'] || $channel['channel_deny_cid'] || $channel['channel_deny_gid']) ? 'lock' : 'unlock'),
-			'bang' => (($group || $cid) ? '!' : ''),
-			'acl' => ((local_user() && local_user() == $owner) ? populate_acl($channel_acl) : ''),
-			'visitor' => true,
-			'profile_uid' => intval($owner),
-			'mimetype' => $mimetype,			
-			'layout' => $layout,
+	if(local_user() && local_user() == $a->profile_uid) {
+		$channel = $a->get_channel();
+		$channel_acl = array(
+			'allow_cid' => $channel['channel_allow_cid'],
+			'allow_gid' => $channel['channel_allow_gid'],
+			'deny_cid'  => $channel['channel_deny_cid'],
+			'deny_gid'  => $channel['channel_deny_gid']
 		);
+	}
+	else
+		$channel_acl = array();
 
-		$o .= status_editor($a,$x);
+	require_once('include/conversation.php');
+	$o = profile_tabs($a,true);
+
+	$o .= '<h2>' . t('Webpages') . '</h2>';
+
+	$x = array(
+		'webpage' => ITEM_WEBPAGE,
+		'is_owner' => true,
+		'nickname' => $a->profile['channel_address'],
+		'lockstate' => (($group || $cid || $channel['channel_allow_cid'] || $channel['channel_allow_gid'] || $channel['channel_deny_cid'] || $channel['channel_deny_gid']) ? 'lock' : 'unlock'),
+		'bang' => (($group || $cid) ? '!' : ''),
+		'acl' => ((local_user() && local_user() == $owner) ? populate_acl($channel_acl) : ''),
+		'visitor' => true,
+		'profile_uid' => intval($owner),
+		'mimetype' => $mimetype,
+		'layout' => $layout,
+	);
+
+	$o .= status_editor($a,$x);
 
 
-//Get a list of webpages.  We can't display all them because endless scroll makes that unusable, so just list titles and an edit link.
-//TODO - this should be replaced with pagelist_widget
+	// Get a list of webpages.  We can't display all them because endless scroll makes that unusable, so just list titles and an edit link.
+	//TODO - this should be replaced with pagelist_widget
 
-	$r = q("select item_id.* from item_id left join item on item_id.iid = item.id where item_id.uid = %d and service = 'WEBPAGE' order by item.created desc",
+	$r = q("select * from item_id left join item on item_id.iid = item.id where item_id.uid = %d and service = 'WEBPAGE' order by item.created desc",
 		intval($owner)
 	);
 
@@ -106,23 +106,28 @@ function webpages_content(&$a) {
 	if($r) {
 		$pages = array();
 		foreach($r as $rr) {
-			$pages[$rr['iid']][] = array('url' => $rr['iid'],'title' => $rr['sid']);
-		} 
+			$pages[$rr['iid']][] = array('url' => $rr['iid'],'pagetitle' => $rr['sid'],'title' => $rr['title'],'created' => datetime_convert('UTC',date_default_timezone_get(),$rr['created']),'edited' => datetime_convert('UTC',date_default_timezone_get(),$rr['edited']));
+		}
 	}
 
 
 //Build the base URL for edit links
-		$url = z_root() . "/editwebpage/" . $which; 
+		$url = z_root() . "/editwebpage/" . $which;
 // This isn't pretty, but it works.  Until I figure out what to do with the UI, it's Good Enough(TM).
-       return $o . replace_macros(get_markup_template("webpagelist.tpl"), array(
+	return $o . replace_macros(get_markup_template("webpagelist.tpl"), array(
 		'$baseurl' => $url,
 		'$edit' => t('Edit'),
 		'$pages' => $pages,
 		'$channel' => $which,
 		'$view' => t('View'),
 		'$preview' => t('Preview'),
-	
-        ));
-    
+		'$actions_txt' => t('Actions'),
+		'$pagelink_txt' => t('Page Link'),
+		'$title_txt' => t('Title'),
+		'$created_txt' => t('Created'),
+		'$edited_txt' => t('Edited')
+
+));
+
 
 }
