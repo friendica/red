b559134a14f1f63ad6be3cc2356a5caa04f554e2
friendica <info@friendica.com>
2014-05-16 02:13:59 -0700

    more doc updates

---
+++ doc/html/include_2apps_8php.js
@@ -0,0 +1,6 @@
+var include_2apps_8php =
+[
+    [ "get_system_apps", "include_2apps_8php.html#ae64f72eb4f126e03b4eb65ed1702a3ca", null ],
+    [ "parse_app_description", "include_2apps_8php.html#a32fe4ed9666228fd4b67dc487cc57a83", null ],
+    [ "translate_system_apps", "include_2apps_8php.html#a48289d5cc44b7638191738106ac5d030", null ]
+];
