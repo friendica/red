8a84718163456efca2e7da151f01698c3d049607
friendica <info@friendica.com>
2014-06-30 22:46:17 -0700

    for non-post items (e.g. photos, events, whatever) add mention tags explicitly to the output rendering since they may not be present in the post body.

--- include/text.php
+++ include/text.php
@@ -1237,6 +1237,27 @@ function format_hashtags(&$item) {
 
 
 
+function format_mentions(&$item) {
+
+	$s = '';
+	$terms = get_terms_oftype($item['term'],TERM_MENTION);
+	if($terms) {
+		$categories = array();
+		foreach($terms as $t) {
+			$term = htmlspecialchars($t['term'],ENT_COMPAT,'UTF-8',false) ;
+			if(! trim($term))
+				continue;
+			if(strpos($item['body'], $t['url']))
+				continue;
+
+			if($s)
+				$s .= '&nbsp';
+
+			$s .= '@<a href="' . zid($t['url']) . '" >' . $term . '</a>';
+		}
+	}
+	return $s;
+}
 
 
 function format_filer(&$item) {
@@ -1292,6 +1313,10 @@ function prepare_body(&$item,$attach = false) {
 
 
 	$s .= format_hashtags($item);
+
+	if($item['resource_type'])
+		$s .= format_mentions($item);
+
 
 	$s .= format_categories($item,$writeable);
 
