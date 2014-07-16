010d4071ada9cf1bd1b709b6f8c83b9006c66d37
Jeroen <jeroen@jeroenpraat.nl>
2014-07-16 18:09:46 +0000

    Introducing page template Redable (sic). A template for making it easier to read long texts on a wide range of devices. Using rem and em for font sizes and region widths. Regions: aside, content, right_aside and footer. Advised to only use the middle content region as much as possible (but left and right can be used for indexes if you like). Based on the default template. Maybe later I will add some alternative css's.

---
+++ view/php/redable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <title><?php if(x($page,'title')) echo $page['title'] ?></title>
+  <script>var baseurl="<?php echo $a->get_baseurl() ?>";</script>
+  <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
+</head>
+<body>
+	<header><?php if(x($page,'header')) echo $page['header']; ?></header>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><?php if(x($page,'nav')) echo $page['nav']; ?></nav>
+	<main>
+		<aside id="region_1"><?php if(x($page,'aside')) echo $page['aside']; ?></aside>
+		<section id="region_2wrap">
+		<section id="region_2"><?php if(x($page,'content')) echo $page['content']; ?>
+					<div id="page-footer"></div>
+			<div id="pause"></div>
+		</section>
+		</section>
+		<aside id="region_3"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></aside>
+	</main>
+	<footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>
+</body>
+</html>
