409c89d629993b59eaafa8443ae9d0b5f0344c1f
friendica <info@friendica.com>
2014-09-09 04:10:01 -0700

    doco correction

--- include/network.php
+++ include/network.php
@@ -133,20 +133,14 @@ function z_fetch_url($url, $binary = false, $redirects = 0, $opts = array()) {
 
 
 /**
- * @function z_fetch_url
+ * @function z_post_url
  * @param string $url
- *    URL to fetch
+ *    URL to post
  * @param mixed $params
- *   The full data to post in a HTTP "POST" operation. To post a file, 
- *   prepend a filename with @ and use the full path. The filetype can
- *   be explicitly specified by following the filename with the type in 
- *   the format ';type=mimetype'. This parameter can either be passed 
- *   as a urlencoded string like 'para1=val1&para2=val2&...' or as an 
- *   array with the field name as key and field data as value. If value 
+ *   The full data to post in a HTTP "POST" operation. This parameter can 
+ *   either be passed as a urlencoded string like 'para1=val1&para2=val2&...' 
+ *   or as an array with the field name as key and field data as value. If value 
  *   is an array, the Content-Type header will be set to multipart/form-data. 
- *   As of PHP 5.2.0, value must be an array if files are passed to this 
- *   option with the @ prefix. As of PHP 5.5.0, the @ prefix is deprecated 
- *   and files can be sent using CURLFile.
  * @param int $redirects = 0
  *    internal use, recursion counter
  * @param array $opts (optional parameters)
