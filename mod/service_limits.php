653ed27fc9640f81b07484b9bed6f6cedfc11b44
friendica <info@friendica.com>
2014-09-11 16:13:44 -0700

    new module - service_limits - print service class limits for logged in account. It is a debugging tool and not meant to be pretty.

---
+++ mod/service_limits.php
@@ -0,0 +1,23 @@
+<?php /** @file */
+
+
+function service_limits_content(&$a) {
+
+	if(! local_user()) {
+		notice( t('Permission denied.') . EOL);
+		return;
+	}
+
+	$account = $a->get_account();
+	if($account['account_service_class']) {
+		$x =  get_config('service_class',$account['account_service_class']);
+		if($x) {
+			$o = print_r($x,true);
+			return $o;
+		}
+	}
+	return t('No service class restrictions found.');
+}
+		
+
+		
