b2fe2b067acc126f5138cbfc31a054ab35447498
friendica <info@friendica.com>
2014-09-14 19:17:05 -0700

    bb2diaspora_itemwallwall not finding author elements - inserting empty bbcode

--- include/bb2diaspora.php
+++ include/bb2diaspora.php
@@ -266,6 +266,10 @@ function bb2dmention_callback($match) {
 function bb2diaspora_itemwallwall(&$item) {
 
 	if(($item['mid'] == $item['parent_mid']) && ($item['author_xchan'] != $item['owner_xchan']) && (is_array($item['author']))) {
+		logger('bb2diaspora_itemwallwall: author: ' . print_r($item['author'],true), LOGGER_DEBUG);
+	}
+
+	if(($item['mid'] == $item['parent_mid']) && ($item['author_xchan'] != $item['owner_xchan']) && (is_array($item['author'])) && $item['author']['url'] && $item['author']['name'] && $item['author']['photo']['src']) {
 		logger('bb2diaspora_itemwallwall: wall to wall post',LOGGER_DEBUG);
 		// post will come across with the owner's identity. Throw a preamble onto the post to indicate the true author.
 		$item['body'] = "\n\n" 
