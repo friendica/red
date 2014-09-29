d33b9e3a91decdc2c6f4dfe438237478ba4b7d56
Jeroen <jeroenpraat@xs4all.nl>
2014-09-29 14:23:26 +0000

    Added 3 new schemas, all named 'simple' in 3 variants. Please enjoy.

---
+++ view/theme/redbasic/schema/simple: black on white.php
@@ -0,0 +1,182 @@
+<?php
+	
+	if (! $nav_bg)
+		$nav_bg = "#fff";
+	if (! $nav_gradient_top)
+		$nav_gradient_top = "#fff";
+	if (! $nav_gradient_bottom)
+		$nav_gradient_bottom = "#fff";
+	if (! $nav_active_gradient_top)
+		$nav_active_gradient_top = "#fff";
+	if (! $nav_active_gradient_bottom)
+		$nav_active_gradient_bottom = "#fff";
+	if (! $nav_bd)
+		$nav_bd = "#fff";
+	if (! $nav_icon_colour)
+		$nav_icon_colour = "#111";
+	if (! $nav_active_icon_colour)
+		$nav_active_icon_colour = "#000";
+	if (! $navmenu_bgchover)
+		$navmenu_bgchover = "#F5F5F5";
+	if (! $navmenu_bgimage)
+		$navmenu_bgimage = "background-image: none;";
+	if (! $navtabs_borderc)
+		$navtabs_borderc = "#111";
+	if (! $navtabs_fontcolour)
+		$navtabs_fontcolour = "#000";
+	if (! $navtabs_bgcolour)
+		$navtabs_bgcolour = "#fff";
+	if (! $navtabs_linkcolour)
+		$navtabs_linkcolour = "color: #000;";
+	if (! $navtabs_linkchover)
+		$navtabs_linkchover = "color: #000;";
+	if (! $navtabs_bgchover)
+		$navtabs_bgchover = "#fff";
+	if (! $navtabs_decohover)
+		$navtabs_decohover = "underline";
+	if (! $link_colour)
+		$link_colour = "#000";
+	if (! $selected_active_colour)
+		$selected_active_colour = "#000";
+	if (! $selected_active_deco)
+		$selected_active_deco = "underline";
+	if (! $banner_colour)
+		$banner_colour = "#fff";
+	if (! $search_background)
+		$search_background = "#F5F5F5";
+	if (! $bgcolour)
+		$bgcolour = "#fff";
+	if (! $item_colour)
+		$item_colour = "rgb(255,255,255)";
+	if (! $comment_item_colour)
+		$comment_item_colour = "rgb(255,255,255)";
+	if (! $comment_border_colour)
+		$comment_border_colour = "rgb(255,255,255)";
+	if (! $toolicon_colour)
+		$toolicon_colour = '#111';
+	if (! $toolicon_activecolour)
+		$toolicon_activecolour = '#000';
+	if (! $font_colour)
+		$font_colour = "#000";
+	if (! $widget_brdrcolour)
+		$widget_brdrcolour = "#fff";
+	if (! $blockquote_colour)
+		$blockquote_colour = "#111";
+	if (! $blockquote_bgcolour)
+		$blockquote_bgcolour = "#fff";
+	if (! $blockquote_bordercolour)
+		$blockquote_bordercolour = "#111";
+	if (! $code_borderc)
+		$code_borderc = "#111";
+	if (! $code_bgcolour)
+		$code_bgcolour = "#fff";
+	if (! $code_txtcolour)
+		$code_txtcolour = "#111";
+	if (! $pre_borderc)
+		$pre_borderc = "#111";
+	if (! $pre_bgcolour)
+		$pre_bgcolour = "#fff";
+	if (! $pre_txtcolour)
+		$pre_txtcolour = "#111";
+	if (! $item_bordercolour)
+		$item_bordercolour = "#000";
+	if (! $notif_itemcolour)
+		$notif_itemcolour = "#000";
+	if (! $notif_itemhovercolour)
+		$notif_itemhovercolour = "#000";
+	if (! $acpopup_bgcolour)
+		$acpopup_bgcolour = "#fff";
+	if (! $acpopup_bordercolour)
+		$acpopup_bordercolour = "#111";
+	if (! $acpopup_tgbl_bgcolour)
+		$acpopup_tgbl_bgcolour = "#fff";
+	if (! $acpopup_hovercolour)
+		$acpopup_hovercolour = "#000";
+	if (! $editbuttons_bgcolour)
+		$editbuttons_bgcolour = "#fff";
+	if (! $editbuttons_bordercolour)
+		$editbuttons_bordercolour = "#fff";
+	if (! $editbuttons_colour)
+		$editbuttons_colour = "#111";
+	if (! $editbuttons_bghover)
+		$editbuttons_bghover = "#fff";
+	if (! $dropdown_bgcolour)
+		$dropdown_bgcolour = "#fff";
+	if (! $dropdown_textcolour)
+		$dropdown_textcolour = "#000";
+	if (! $dropdown_txtcolhover)
+		$dropdown_txtcolhover = "#000";
+	if (! $dropdown_bgcolhover)
+		$dropdown_bgcolhover = "#F5F5F5";
+	if (! $dropdown_bgimghover)
+		$dropdown_bgimghover = "background-image: none;";
+	if (! $dropdown_togglecol)
+		$dropdown_togglecol = "#000";
+	if (! $dropdown_togglebgcol)
+		$dropdown_togglebgcol = "#fff";
+	if (! $dropdown_bordercol)
+		$dropdown_bordercol = "#fff";
+	if (! $preview_backgroundimg)
+		$preview_backgroundimg = "gray_and_black_diagonal_stripes_background_seamless.gif";
+	if (! $notify_bgcolour)
+		$notify_bgcolour = "#fff";
+	if (! $notify_linkcolour)
+		$notify_linkcolour = "#000";	
+	if (! $notify_bghover)
+		$notify_bghover = "#fff";
+	if (! $notify_topmargin)
+		$notify_topmargin = "-1px";
+	if (! $notifyseen_bgcolour)
+		$notifyseen_bgcolour = "#fff";
+	if (! $notifyseen_linkcolour)
+		$notifyseen_linkcolour = "#fff";
+	if (! $notifyseen_bghover)
+		$notifyseen_bghover = "#fff";
+	if (! $notifyseen_linkhover)
+		$notifyseen_linkhover = "#000";
+	if (! $input_bgsubmit)
+		$input_bgsubmit = "#fff";
+	if (! $input_linksubmit)
+		$input_linksubmit = "#000";
+	if (! $input_border)
+		$input_border = "#fff";
+	if (! $input_colourhover)
+		$input_colourhover = "#000";
+	if (! $input_decohover)
+		$input_decohover = "underline";
+	if (! $acl_bgcolour)
+		$acl_bgcolour = "#fff";
+	if (! $acl_bordercolour)
+		$acl_bordercolour = "#fff";
+	if (! $aclbutton_linkcolour)
+		$aclbutton_linkcolour = "color: #000;";
+	if (! $abookself_bgcolour)
+		$abookself_bgcolour = "#fff";
+	if(! $infomess_bgcolour)
+		$infomess_bgcolour = "#fff";
+	if(! $alert_txtcolour)
+      $alert_txtcolour = "#000";	
+	if(! $alert_bgcolour)
+      $alert_bgcolour = "#fff";
+   if(! $alert_bordercol)
+      $alert_bordercol = "#fff";
+   if(! $alert_gradientcol)
+      $alert_gradientcol = "#fff";		
+   if(! $advperm_bgcolour)
+      $advperm_bgcolour = "#fff";
+   if(! $advperm_bordercol)
+      $advperm_bordercol = "#fff";
+   if(! $advperm_gradientcol)
+      $advperm_gradientcol = "#fff";
+   if(! $cal_bgcolour)
+      $cal_bgcolour = "#fff";
+   if(! $fancybox_bgcolour)
+      $fancybox_bgcolour = "#fff";
+	if (!$admintable_hoverbgcol)
+		$admintable_hoverbgcol="#fff";
+	if (!$dirpopup_txtcol)
+		$dirpopup_txtcol="#fff";
+	if (!$dirpopup_linkcol)
+		$dirpopup_linkcol="#fff";
+	if (!$abook_changebg)
+		$abook_changebg="#fff";
---
+++ view/theme/redbasic/schema/simple: white on black.php
@@ -0,0 +1,182 @@
+<?php
+	
+	if (! $nav_bg)
+		$nav_bg = "#000";
+	if (! $nav_gradient_top)
+		$nav_gradient_top = "#000";
+	if (! $nav_gradient_bottom)
+		$nav_gradient_bottom = "#000";
+	if (! $nav_active_gradient_top)
+		$nav_active_gradient_top = "#000";
+	if (! $nav_active_gradient_bottom)
+		$nav_active_gradient_bottom = "#000";
+	if (! $nav_bd)
+		$nav_bd = "#000";
+	if (! $nav_icon_colour)
+		$nav_icon_colour = "#eee";
+	if (! $nav_active_icon_colour)
+		$nav_active_icon_colour = "#fff";
+	if (! $navmenu_bgchover)
+		$navmenu_bgchover = "#030303";
+	if (! $navmenu_bgimage)
+		$navmenu_bgimage = "background-image: none;";
+	if (! $navtabs_borderc)
+		$navtabs_borderc = "#eee";
+	if (! $navtabs_fontcolour)
+		$navtabs_fontcolour = "#fff";
+	if (! $navtabs_bgcolour)
+		$navtabs_bgcolour = "#000";
+	if (! $navtabs_linkcolour)
+		$navtabs_linkcolour = "color: #fff;";
+	if (! $navtabs_linkchover)
+		$navtabs_linkchover = "color: #fff;";
+	if (! $navtabs_bgchover)
+		$navtabs_bgchover = "#000";
+	if (! $navtabs_decohover)
+		$navtabs_decohover = "underline";
+	if (! $link_colour)
+		$link_colour = "#fff";
+	if (! $selected_active_colour)
+		$selected_active_colour = "#fff";
+	if (! $selected_active_deco)
+		$selected_active_deco = "underline";
+	if (! $banner_colour)
+		$banner_colour = "#000";
+	if (! $search_background)
+		$search_background = "#000";
+	if (! $bgcolour)
+		$bgcolour = "#000";
+	if (! $item_colour)
+		$item_colour = "rgb(0,0,0)";
+	if (! $comment_item_colour)
+		$comment_item_colour = "rgb(0,0,0)";
+	if (! $comment_border_colour)
+		$comment_border_colour = "rgb(0,0,0)";
+	if (! $toolicon_colour)
+		$toolicon_colour = '#eee';
+	if (! $toolicon_activecolour)
+		$toolicon_activecolour = '#fff';
+	if (! $font_colour)
+		$font_colour = "#fff";
+	if (! $widget_brdrcolour)
+		$widget_brdrcolour = "#000";
+	if (! $blockquote_colour)
+		$blockquote_colour = "#eee";
+	if (! $blockquote_bgcolour)
+		$blockquote_bgcolour = "#000";
+	if (! $blockquote_bordercolour)
+		$blockquote_bordercolour = "#eee";
+	if (! $code_borderc)
+		$code_borderc = "#eee";
+	if (! $code_bgcolour)
+		$code_bgcolour = "#000";
+	if (! $code_txtcolour)
+		$code_txtcolour = "#eee";
+	if (! $pre_borderc)
+		$pre_borderc = "#eee";
+	if (! $pre_bgcolour)
+		$pre_bgcolour = "#000";
+	if (! $pre_txtcolour)
+		$pre_txtcolour = "#eee";
+	if (! $item_bordercolour)
+		$item_bordercolour = "#fff";
+	if (! $notif_itemcolour)
+		$notif_itemcolour = "#fff";
+	if (! $notif_itemhovercolour)
+		$notif_itemhovercolour = "#fff";
+	if (! $acpopup_bgcolour)
+		$acpopup_bgcolour = "#000";
+	if (! $acpopup_bordercolour)
+		$acpopup_bordercolour = "#eee";
+	if (! $acpopup_tgbl_bgcolour)
+		$acpopup_tgbl_bgcolour = "#000";
+	if (! $acpopup_hovercolour)
+		$acpopup_hovercolour = "#fff";
+	if (! $editbuttons_bgcolour)
+		$editbuttons_bgcolour = "#000";
+	if (! $editbuttons_bordercolour)
+		$editbuttons_bordercolour = "#000";
+	if (! $editbuttons_colour)
+		$editbuttons_colour = "#eee";
+	if (! $editbuttons_bghover)
+		$editbuttons_bghover = "#000";
+	if (! $dropdown_bgcolour)
+		$dropdown_bgcolour = "#000";
+	if (! $dropdown_textcolour)
+		$dropdown_textcolour = "#fff";
+	if (! $dropdown_txtcolhover)
+		$dropdown_txtcolhover = "#fff";
+	if (! $dropdown_bgcolhover)
+		$dropdown_bgcolhover = "#030303";
+	if (! $dropdown_bgimghover)
+		$dropdown_bgimghover = "background-image: none;";
+	if (! $dropdown_togglecol)
+		$dropdown_togglecol = "#fff";
+	if (! $dropdown_togglebgcol)
+		$dropdown_togglebgcol = "#000";
+	if (! $dropdown_bordercol)
+		$dropdown_bordercol = "#000";
+	if (! $preview_backgroundimg)
+		$preview_backgroundimg = "gray_and_black_diagonal_stripes_background_seamless.gif";
+	if (! $notify_bgcolour)
+		$notify_bgcolour = "#000";
+	if (! $notify_linkcolour)
+		$notify_linkcolour = "#fff";	
+	if (! $notify_bghover)
+		$notify_bghover = "#000";
+	if (! $notify_topmargin)
+		$notify_topmargin = "-1px";
+	if (! $notifyseen_bgcolour)
+		$notifyseen_bgcolour = "#000";
+	if (! $notifyseen_linkcolour)
+		$notifyseen_linkcolour = "#000";
+	if (! $notifyseen_bghover)
+		$notifyseen_bghover = "#000";
+	if (! $notifyseen_linkhover)
+		$notifyseen_linkhover = "#fff";
+	if (! $input_bgsubmit)
+		$input_bgsubmit = "#000";
+	if (! $input_linksubmit)
+		$input_linksubmit = "#fff";
+	if (! $input_border)
+		$input_border = "#000";
+	if (! $input_colourhover)
+		$input_colourhover = "#fff";
+	if (! $input_decohover)
+		$input_decohover = "underline";
+	if (! $acl_bgcolour)
+		$acl_bgcolour = "#000";
+	if (! $acl_bordercolour)
+		$acl_bordercolour = "#000";
+	if (! $aclbutton_linkcolour)
+		$aclbutton_linkcolour = "color: #fff;";
+	if (! $abookself_bgcolour)
+		$abookself_bgcolour = "#000";
+	if(! $infomess_bgcolour)
+		$infomess_bgcolour = "#000";
+	if(! $alert_txtcolour)
+      $alert_txtcolour = "#fff";	
+	if(! $alert_bgcolour)
+      $alert_bgcolour = "#000";
+   if(! $alert_bordercol)
+      $alert_bordercol = "#000";
+   if(! $alert_gradientcol)
+      $alert_gradientcol = "#000";		
+   if(! $advperm_bgcolour)
+      $advperm_bgcolour = "#000";
+   if(! $advperm_bordercol)
+      $advperm_bordercol = "#000";
+   if(! $advperm_gradientcol)
+      $advperm_gradientcol = "#000";
+   if(! $cal_bgcolour)
+      $cal_bgcolour = "#000";
+   if(! $fancybox_bgcolour)
+      $fancybox_bgcolour = "#000";
+	if (!$admintable_hoverbgcol)
+		$admintable_hoverbgcol="#000";
+	if (!$dirpopup_txtcol)
+		$dirpopup_txtcol="#000";
+	if (!$dirpopup_linkcol)
+		$dirpopup_linkcol="#000";
+	if (!$abook_changebg)
+		$abook_changebg="#000";
