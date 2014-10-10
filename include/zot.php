f26cd5a636d0a1de4cef66d26da505e8a29b1b40
friendica <info@friendica.com>
2014-10-10 15:17:32 -0700

    ah yes, the redundant relay run was for the benefit of wall-to-wall posts issue #633

--- include/zot.php
+++ include/zot.php
@@ -1480,18 +1480,17 @@ function process_delivery($sender,$arr,$deliveries,$relay,$public = false) {
 			intval($channel['channel_id'])
 		);
 		if($r) {
+			$item_id = $r[0]['id'];
 			if($arr['edited'] > $r[0]['edited']) {
 				$arr['id'] = $r[0]['id'];
 				$arr['uid'] = $channel['channel_id'];
 				update_imported_item($sender,$arr,$channel['channel_id']);
 				$result[] = array($d['hash'],'updated',$channel['channel_name'] . ' <' . $channel['channel_address'] . '@' . get_app()->get_hostname() . '>',$arr['mid']);
-				$item_id = $r[0]['id'];
 				if(! $relay)
 					add_source_route($item_id,$sender['hash']);
 			}
 			else {
 				$result[] = array($d['hash'],'update ignored',$channel['channel_name'] . ' <' . $channel['channel_address'] . '@' . get_app()->get_hostname() . '>',$arr['mid']);
-				continue;
 			}
 		}
 		else {
