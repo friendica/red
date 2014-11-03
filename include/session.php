9984ff37e5df237ccdd0169387ade070babfd666
Thomas Willingham <beardyunixer@beardyunixer.com>
2014-11-03 15:33:59 +0000

    innodb issue

--- include/session.php
+++ include/session.php
@@ -84,8 +84,9 @@ function ref_session_destroy ($id) {
 
 function ref_session_gc($expire) {
   q("DELETE FROM session WHERE expire < %d", dbesc(time()));
-  q("OPTIMIZE TABLE session");
-  return true;
+    if (! get_config('system','innodb'))
+        q("OPTIMIZE TABLE session");
+    return true;
 }
 
 $gc_probability = 50;
