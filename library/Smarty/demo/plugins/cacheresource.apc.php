967ab871b836f618107fe144978bd1453c3c6634
Thomas Willingham <beardyunixer@beardyunixer.com>
2014-12-11 20:15:27 +0000

    Update Smarty

--- library/Smarty/demo/plugins/cacheresource.apc.php
+++ library/Smarty/demo/plugins/cacheresource.apc.php
@@ -2,19 +2,19 @@
 
 /**
  * APC CacheResource
- *
  * CacheResource Implementation based on the KeyValueStore API to use
  * memcache as the storage resource for Smarty's output caching.
  * *
+ *
  * @package CacheResource-examples
- * @author Uwe Tews
+ * @author  Uwe Tews
  */
-class Smarty_CacheResource_Apc extends Smarty_CacheResource_KeyValueStore {
-
+class Smarty_CacheResource_Apc extends Smarty_CacheResource_KeyValueStore
+{
     public function __construct()
     {
         // test if APC is present
-        if(!function_exists('apc_cache_info')) {
+        if (!function_exists('apc_cache_info')) {
             throw new Exception('APC Template Caching Error: APC is not installed');
         }
     }
@@ -22,8 +22,9 @@ class Smarty_CacheResource_Apc extends Smarty_CacheResource_KeyValueStore {
     /**
      * Read values for a set of keys from cache
      *
-     * @param array $keys list of keys to fetch
-     * @return array list of values with the given keys used as indexes
+     * @param  array $keys list of keys to fetch
+     *
+     * @return array   list of values with the given keys used as indexes
      * @return boolean true on success, false on failure
      */
     protected function read(array $keys)
@@ -33,28 +34,32 @@ class Smarty_CacheResource_Apc extends Smarty_CacheResource_KeyValueStore {
         foreach ($res as $k => $v) {
             $_res[$k] = $v;
         }
+
         return $_res;
     }
 
     /**
      * Save values for a set of keys to cache
      *
-     * @param array $keys list of values to save
-     * @param int $expire expiration time
+     * @param  array $keys   list of values to save
+     * @param  int   $expire expiration time
+     *
      * @return boolean true on success, false on failure
      */
-    protected function write(array $keys, $expire=null)
+    protected function write(array $keys, $expire = null)
     {
         foreach ($keys as $k => $v) {
             apc_store($k, $v, $expire);
         }
+
         return true;
     }
 
     /**
      * Remove values from cache
      *
-     * @param array $keys list of keys to delete
+     * @param  array $keys list of keys to delete
+     *
      * @return boolean true on success, false on failure
      */
     protected function delete(array $keys)
@@ -62,6 +67,7 @@ class Smarty_CacheResource_Apc extends Smarty_CacheResource_KeyValueStore {
         foreach ($keys as $k) {
             apc_delete($k);
         }
+
         return true;
     }
 
