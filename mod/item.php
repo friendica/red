dfdef0af0d045bdb0eb6e5ed557579966891e1a0
friendica <info@friendica.com>
2015-01-13 18:30:30 -0800

    provide a setting to control ALLOWCODE permissions at the channel level - it isn't always appropriate to apply this to all channels in an account.

--- mod/item.php
+++ mod/item.php
@@ -449,10 +449,10 @@ function item_post(&$a) {
 	$execflag = false;
 
 	if($mimetype === 'application/x-php') {
-		$z = q("select account_id, account_roles from account left join channel on channel_account_id = account_id where channel_id = %d limit 1",
+		$z = q("select account_id, account_roles, channel_pageflags from account left join channel on channel_account_id = account_id where channel_id = %d limit 1",
 			intval($profile_uid)
 		);
-		if($z && ($z[0]['account_roles'] & ACCOUNT_ROLE_ALLOWCODE)) {
+		if($z && (($z[0]['account_roles'] & ACCOUNT_ROLE_ALLOWCODE) || ($z[0]['channel_pageflags'] & PAGE_ALLOWCODE))) {
 			if($uid && (get_account_id() == $z[0]['account_id'])) {
 				$execflag = true;
 			}
