b9a1b01c923c14848ece9aae55a435b4bba816a8
Mario Vavti <mario@mariovavti.com>
2015-03-05 12:52:09 +0100

    fix theme preview image css

--- view/js/main.js
+++ view/js/main.js
@@ -1141,7 +1141,7 @@ Array.prototype.remove = function(item) {
 function previewTheme(elm) {
 	theme = $(elm).val();
 	$.getJSON('pretheme?f=&theme=' + theme,function(data) {
-			$('#theme-preview').html('<div id="theme-desc">' + data.desc + '</div><div id="theme-version">' + data.version + '</div><div id="theme-credits">' + data.credits + '</div><a href="' + data.img + '"><img src="' + data.img + '" width="320" height="240" alt="' + theme + '" /></a>');
+			$('#theme-preview').html('<div id="theme-desc">' + data.desc + '</div><div id="theme-version">' + data.version + '</div><div id="theme-credits">' + data.credits + '</div><a href="' + data.img + '"><img src="' + data.img + '" style="max-width:100%;" alt="' + theme + '" /></a>');
 	});
 
 }
