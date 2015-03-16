ccf4734f7bb525ae6b1ebe63e3a2b6b00c4f823b
friendica <info@friendica.com>
2015-03-16 15:50:45 -0700

    make sure this sucker is quiet

--- mod/wall_attach.php
+++ mod/wall_attach.php
@@ -22,7 +22,7 @@ function wall_attach_post(&$a) {
 
 
 	if($_FILES['userfile']['tmp_name']) {
-		$x = getimagesize($_FILES['userfile']['tmp_name']);
+		$x = @getimagesize($_FILES['userfile']['tmp_name']);
 		if(($x) && ($x[2] === IMG_GIF || $x[2] === IMG_JPG || $x[2] === IMG_JPEG || $x[2] === IMG_PNG)) {
 			$args = array( 'source' => 'editor', 'visible' => 0, 'contact_allow' => array($channel['channel_hash']));
 			$ret = photo_upload($channel,$observer,$args);
